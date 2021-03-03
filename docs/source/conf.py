72dd2fba2ad1c521c650483ee09245880dec7165
Patrick Reiser <64005383+PatReis@users.noreply.github.com>
2021-03-03 15:05:37 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,9 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
 
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -31,7 +32,7 @@ release = '0.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-import sphinx_rtd_theme
+
 
 extensions = [
     'sphinx.ext.todo',
