990579c2b68db15f5ab4a93ea43b3f1c3efb416e
Patrick Reiser <64005383+PatReis@users.noreply.github.com>
2021-03-03 12:26:00 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,8 +30,18 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.napoleon'
+import sphinx_rtd_theme
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+    'sphinx.ext.napoleon'
 ]
+
 napoleon_google_docstring = True
 
 # Add any paths that contain templates here, relative to this directory.
