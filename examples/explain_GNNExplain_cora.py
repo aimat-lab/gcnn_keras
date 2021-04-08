f6d8f4a67bd0f42381af34c555bc310bc2b0b37b
cm5828 <patrick.reiser@kit.edu>
2021-04-08 12:11:04 +0200

    make example cora

--- examples/explain_GNNExplain_cora.py
+++ examples/explain_GNNExplain_cora.py
@@ -1,20 +1,38 @@
-import tensorflow as tf
-from sklearn.model_selection import train_test_split
+import time
+
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
-
-from kgcnn.literature.GNNExplain import GNNExplainer,GNNExplainerOptimizer,GNNInterface
-
-from kgcnn.utils.adj import precompute_adjacency_scaled,sort_edge_indices,make_adjacency_from_edge_indices,make_adjacency_undirected_logical_or,convert_scaled_adjacency_to_list
-from kgcnn.utils.data import ragged_tensor_from_nested_numpy
-from kgcnn.literature.GCN import make_gcn
-from kgcnn.utils.learning import lr_lin_reduction
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
 
 from kgcnn.data.cora.cora_lu import cora_graph
+from kgcnn.literature.GCN import make_gcn
+from kgcnn.literature.GNNExplain import GNNExplainer, GNNInterface
+from kgcnn.utils.adj import precompute_adjacency_scaled, sort_edge_indices, make_adjacency_from_edge_indices, \
+    make_adjacency_undirected_logical_or, convert_scaled_adjacency_to_list
+from kgcnn.utils.data import ragged_tensor_from_nested_numpy
+from kgcnn.utils.learning import lr_lin_reduction
+
+
+# Find a color to visualize a label
+def get_label_color(label):
+    return plt.get_cmap('Set1')(label / 7)
+
+
+# Map label to class
+def get_label_name(label):
+    return ["Case_Based",
+            "Genetic_Algorithms",
+            "Neural_Networks",
+            "Probabilistic_Methods",
+            "Reinforcement_Learning",
+            "Rule_Learning",
+            "Theory"][label]
+
 
 nodes, edge_index, labels, class_label_mapping = cora_graph()
-nodes = nodes [:,1:] # Remove IDs
+nodes = nodes[:, 1:]  # Remove IDs
 edge_index = sort_edge_indices(edge_index)
 adj_matrix = make_adjacency_from_edge_indices(edge_index)
 adj_matrix = precompute_adjacency_scaled(make_adjacency_undirected_logical_or(adj_matrix))
@@ -27,9 +45,9 @@ labels = np.array(labels == np.arange(7), dtype=np.float)
 # Since only one graph in the dataset
 # Use a mask to hide test nodes labels
 inds = np.arange(len(labels))
-ind_train, ind_val = train_test_split(inds, test_size=0.10, random_state=42)
-val_mask = np.zeros_like(labels)
-train_mask = np.zeros_like(labels)
+ind_train, ind_val = train_test_split(inds, test_size=0.10, random_state=0)
+val_mask = np.zeros_like(inds)
+train_mask = np.zeros_like(inds)
 val_mask[ind_val] = 1
 train_mask[ind_train] = 1
 val_mask = np.expand_dims(val_mask, axis=0)  # One graph in batch
@@ -44,7 +62,7 @@ xtrain = nodes, edges, edge_indices
 ytrain = np.expand_dims(labels, axis=0)  # One graph in batch
 
 model = make_gcn(
-    input_node_shape=[None, 1432],
+    input_node_shape=[None, 1433],
     input_edge_shape=[None, 1],
     # Output
     output_embedd={"output_mode": 'node'},
@@ -68,3 +86,185 @@ model.compile(loss='categorical_crossentropy',
               optimizer=optimizer,
               weighted_metrics=['categorical_accuracy'])
 print(model.summary())
+
+# Training loop
+trainlossall = []
+testlossall = []
+start = time.process_time()
+for iepoch in range(0, epo, epostep):
+    hist = model.fit(xtrain, ytrain,
+                     epochs=iepoch + epostep,
+                     initial_epoch=iepoch,
+                     batch_size=1,
+                     callbacks=[cbks],
+                     verbose=1,
+                     sample_weight=train_mask  # Important!!!
+                     )
+
+    trainlossall.append(hist.history)
+    testlossall.append(model.evaluate(xtrain, ytrain, sample_weight=val_mask))
+stop = time.process_time()
+print("Print Time for taining: ", stop - start)
+
+# Pick out accuracy
+testlossall = np.array(testlossall)
+trainlossall = np.concatenate([x['categorical_accuracy'] for x in trainlossall])
+
+# Plot loss vs epochs
+plt.figure()
+plt.plot(np.arange(1, len(trainlossall) + 1), trainlossall, label='Training Loss', c='blue')
+plt.plot(np.arange(epostep, epo + epostep, epostep), testlossall[:, 1], label='Test Loss', c='red')
+plt.xlabel('Epochs')
+plt.ylabel('Accurarcy')
+plt.title('GCN')
+plt.legend(loc='lower right', fontsize='x-large')
+plt.savefig('gcn_explain_loss.png')
+plt.show()
+
+# Evaluate model
+model.evaluate(xtrain, ytrain, sample_weight=val_mask)
+
+
+# We have to implement the GNNexplainer for the model
+class ExplainableGCN(GNNInterface):
+
+    def __init__(self, gnn_model, node_index, **kwargs):
+        super(ExplainableGCN, self).__init__()
+        self.gnn_model = gnn_model
+        self.node_index = node_index
+
+    def predict(self, gnn_input, masking_info=None):
+        return tf.expand_dims(self.gnn_model(gnn_input, training=False)[0][self.node_index], 0)
+
+    def masked_predict(self, gnn_input, edge_mask, feature_mask, node_mask, training=False):
+        node_input, edge_input, edge_index_input = gnn_input
+
+        masked_edge_input = tf.ragged.map_flat_values(tf.math.multiply, tf.dtypes.cast(edge_input, tf.float32),
+                                                      edge_mask)
+        masked_feature_input = tf.ragged.map_flat_values(tf.math.multiply, tf.dtypes.cast(node_input, tf.float32),
+                                                         tf.transpose(feature_mask))
+        masked_pred = tf.expand_dims(
+            self.gnn_model([masked_feature_input, masked_edge_input, edge_index_input], training=training)[0][
+                self.node_index], 0)
+        return masked_pred
+
+    def get_number_of_nodes(self, gnn_input):
+        node_input, _, _ = gnn_input
+        return node_input[0].shape[0]
+
+    def get_number_of_node_features(self, gnn_input):
+        node_input, _, _ = gnn_input
+        return node_input[0].shape[1]
+
+    def get_number_of_edges(self, gnn_input):
+        _, edge_input, _ = gnn_input
+        return edge_input[0].shape[0]
+
+    def get_explanation(self, gnn_input, edge_mask, feature_mask, node_mask, node_labels=None):
+        edge_relevance = np.array(edge_mask[:, 0])
+        node_relevance = np.array(node_mask[:, 0])
+        features = np.array(gnn_input[0][0])
+        edges = np.array(gnn_input[2][0])
+        g = nx.Graph()
+        for i, f in enumerate(features):
+            if node_labels is None:
+                g.add_node(i, features=f, relevance=node_relevance[i])
+            else:
+                g.add_node(i, features=f, relevance=node_relevance[i], label=node_labels[i])
+        for i, e in enumerate(edges):
+            if edge_relevance is None:
+                g.add_edge(e[0], e[1])
+            else:
+                g.add_edge(e[0], e[1], relevance=edge_relevance[i])
+        return g
+
+    def present_explanation(self, explanation, threshold=0.5):
+        relevant_edges = []
+        edge_color_map = []
+        for (v, u, relevance) in explanation.edges.data('relevance'):
+            if relevance > threshold:
+                relevant_edges.append((v, u))
+                edge_color_map.append((0, 0, 0, relevance))
+        subgraph = explanation.edge_subgraph(relevant_edges)
+        node_colors = []
+        for n, l in subgraph.nodes.data('label'):
+            r, g, b, a = get_label_color(l)
+            a = subgraph.nodes[n]['relevance']
+            node_colors.append((r, g, b, a))
+        nx.draw(subgraph, node_color=node_colors, edge_color=edge_color_map, with_labels=True)
+
+
+# Helper functions
+def get_output_tensor_for_label(label):
+    assert 0 <= label <= 6
+    output_to_explain = np.zeros(7)
+    output_to_explain[label] = 1.
+    output_to_explain = np.expand_dims(output_to_explain, 0)
+    return tf.Variable(output_to_explain)
+
+
+def get_label_from_tensor(tensor_output):
+    return np.argmax(tensor_output)
+
+
+# Choose a node to explain and instantiate an explainable GNN:
+node_index = np.argwhere(val_mask[0] == 1)[:, 0][0]
+# node_index = 9
+explainable_gcn = ExplainableGCN(model, node_index)
+
+# Get the prediction of the GNN for the chosen node:
+prediction = explainable_gcn.predict(xtrain)
+predicted_label = get_label_from_tensor(prediction)
+print('predicted label: ', predicted_label, get_label_name(predicted_label))
+print("true label:", get_label_from_tensor(labels[node_index]))
+print('node_index: ', node_index)
+
+# Setup the GNNExplainer:
+gnnexplaineroptimizer_options = {'edge_mask_loss_weight': 0.001,
+                                 'edge_mask_norm_ord': 2,
+                                 'feature_mask_loss_weight': 0,
+                                 'feature_mask_norm_ord': 2,
+                                 'node_mask_loss_weight': 0,
+                                 'node_mask_norm_ord': 1}
+compile_options = {'loss': 'categorical_crossentropy', 'optimizer': tf.keras.optimizers.Adam(lr=1)}
+fit_options = {'epochs': 80, 'verbose': 0}
+
+explainer = GNNExplainer(explainable_gcn,
+                         compile_options=compile_options,
+                         fit_options=fit_options,
+                         gnnexplaineroptimizer_options=gnnexplaineroptimizer_options)
+
+# Explain the instance with index node_index:
+# inspection_result = explainer.explain(x, output_to_explain=get_output_tensor_for_label(1), inspection=True)
+inspection_result = explainer.explain(xtrain, inspection=True)
+
+# Present the explanation
+plt.figure()
+explainer.present_explanation(explainer.get_explanation(node_labels=np.argmax(ytrain[0], axis=1)), threshold=0.1)
+plt.show()
+
+# Plot the inspection_result to see if we chose suitable parameters:
+plt.figure()
+for i in range(7):
+    plt.plot(np.array(inspection_result['predictions'])[:, i], color=get_label_color(i))
+plt.show()
+
+# Plot Loss
+plt.figure()
+plt.plot(np.array(inspection_result['total_loss']), color="black")
+plt.show()
+
+# For comparison, the 2-hop graph around the chosen instance:
+plt.figure()
+cora_graph = nx.Graph()
+cora_graph.add_nodes_from([(i, {"label": labels[i]}) for i in inds])
+cora_graph.add_edges_from(edge_index)
+hops = 2
+khopgraph = nx.generators.ego.ego_graph(cora_graph, node_index, radius=hops)
+for n in khopgraph.nodes:
+    khopgraph.nodes[n]['label'] = np.argmax(khopgraph.nodes[n]['label'])
+    khopgraph.nodes[n]['relevance'] = 1.
+for (u, v) in khopgraph.edges:
+    khopgraph.edges[u, v]['relevance'] = 1.0
+explainer.present_explanation(khopgraph)
+plt.show()
