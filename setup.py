f4f68579c00df497e04e33276bf9ea06e03ecb9d
cm5828 <patrick.reiser@kit.edu>
2021-04-10 12:33:35 +0200

    start new update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="kgcnn",
-    version="0.1.1",
+    version="0.1.2",
     author="Patrick Reiser",
     author_email="patrick.reiser@kit.edu",
     description="General Base Layers for Graph Convolutions with tensorflow.keras",
