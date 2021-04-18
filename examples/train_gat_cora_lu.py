382ec01d4ab6b8ecd7d9af9e723d103d5eee342a
cm5828 <patrick.reiser@kit.edu>
2021-04-18 15:11:50 +0200

    added GAT model and train on cora_lu

---
+++ examples/train_gat_cora_lu.py
@@ -0,0 +1,121 @@
+import matplotlib.pyplot as plt
+import time
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+
+from kgcnn.literature.GAT import make_gat
+from kgcnn.data.cora.cora_lu import cora_graph
+from kgcnn.utils.adj import precompute_adjacency_scaled, sort_edge_indices, make_adjacency_from_edge_indices, \
+    make_adjacency_undirected_logical_or, convert_scaled_adjacency_to_list
+from kgcnn.utils.data import ragged_tensor_from_nested_numpy
+from kgcnn.utils.learning import lr_lin_reduction
+
+
+# Find a color to visualize a label
+def get_label_color(label):
+    return plt.get_cmap('Set1')(label / 7)
+
+
+# Map label to class
+def get_label_name(label):
+    return ["Case_Based",
+            "Genetic_Algorithms",
+            "Neural_Networks",
+            "Probabilistic_Methods",
+            "Reinforcement_Learning",
+            "Rule_Learning",
+            "Theory"][label]
+
+
+nodes, edge_index, labels, class_label_mapping = cora_graph()
+nodes = nodes[:, 1:]  # Remove IDs
+edge_index = sort_edge_indices(edge_index)
+adj_matrix = make_adjacency_from_edge_indices(edge_index)
+adj_matrix = precompute_adjacency_scaled(make_adjacency_undirected_logical_or(adj_matrix))
+edge_index, edge_weight = convert_scaled_adjacency_to_list(adj_matrix)
+edge_weight = np.expand_dims(edge_weight, axis=-1)
+labels = np.expand_dims(labels, axis=-1)
+labels = np.array(labels == np.arange(7), dtype=np.float)
+
+# Make test/train split
+# Since only one graph in the dataset
+# Use a mask to hide test nodes labels
+inds = np.arange(len(labels))
+ind_train, ind_val = train_test_split(inds, test_size=0.10, random_state=0)
+val_mask = np.zeros_like(inds)
+train_mask = np.zeros_like(inds)
+val_mask[ind_val] = 1
+train_mask[ind_train] = 1
+val_mask = np.expand_dims(val_mask, axis=0)  # One graph in batch
+train_mask = np.expand_dims(train_mask, axis=0)  # One graph in batch
+
+# Make ragged graph tensors with 1 graph in batch
+nodes, edges, edge_indices = ragged_tensor_from_nested_numpy([nodes]), ragged_tensor_from_nested_numpy(
+    [edge_weight]), ragged_tensor_from_nested_numpy([edge_index])  # One graph in batch
+
+# Set training data. But requires mask and batch-dimension of 1
+xtrain = nodes, edges, edge_indices
+ytrain = np.expand_dims(labels, axis=0)  # One graph in batch
+
+model = make_gat(
+    input_node_shape=[None, 1433],
+    input_edge_shape=[None, 1],
+    # Output
+    output_embedd={"output_mode": 'node'},
+    output_mlp={"use_bias": [True, True, False], "units": [64, 32, 7], "activation": ['relu', 'relu', 'softmax']},
+    # model specs
+    depth=3,
+    attention_heads_num=10,
+    attention_heads_concat=False,
+    attention_args={"units": 32, "use_bias": True, "has_unconnected": True, "use_edge_features": True}
+)
+
+# Set learning rate and epochs
+learning_rate_start = 1e-3
+learning_rate_stop = 1e-4
+epo = 250
+epomin = 200
+epostep = 10
+
+# Compile model with optimizer and loss
+optimizer = tf.keras.optimizers.Adam(lr=learning_rate_start)
+cbks = tf.keras.callbacks.LearningRateScheduler(lr_lin_reduction(learning_rate_start, learning_rate_stop, epomin, epo))
+model.compile(loss='categorical_crossentropy',
+              optimizer=optimizer,
+              weighted_metrics=['categorical_accuracy'])
+print(model.summary())
+
+# Training loop
+trainlossall = []
+testlossall = []
+start = time.process_time()
+for iepoch in range(0, epo, epostep):
+    hist = model.fit(xtrain, ytrain,
+                     epochs=iepoch + epostep,
+                     initial_epoch=iepoch,
+                     batch_size=1,
+                     callbacks=[cbks],
+                     verbose=1,
+                     sample_weight=train_mask  # Important!!!
+                     )
+
+    trainlossall.append(hist.history)
+    testlossall.append(model.evaluate(xtrain, ytrain, sample_weight=val_mask))
+stop = time.process_time()
+print("Print Time for taining: ", stop - start)
+
+# Pick out accuracy
+testlossall = np.array(testlossall)
+trainlossall = np.concatenate([x['categorical_accuracy'] for x in trainlossall])
+
+# Plot loss vs epochs
+plt.figure()
+plt.plot(np.arange(1, len(trainlossall) + 1), trainlossall, label='Training Loss', c='blue')
+plt.plot(np.arange(epostep, epo + epostep, epostep), testlossall[:, 1], label='Test Loss', c='red')
+plt.xlabel('Epochs')
+plt.ylabel('Accurarcy')
+plt.title('GCN')
+plt.legend(loc='lower right', fontsize='x-large')
+plt.savefig('gcn_gat_loss_cora.png')
+plt.show()
---
+++ kgcnn/literature/GAT.py
@@ -0,0 +1,92 @@
+import tensorflow as tf
+import tensorflow.keras as ks
+from kgcnn.layers.disjoint.casting import CastRaggedToDisjoint, CastValuesToRagged
+from kgcnn.layers.disjoint.mlp import MLP
+from kgcnn.layers.disjoint.pooling import PoolingNodes
+from kgcnn.layers.ragged.casting import CastRaggedToDense
+from kgcnn.utils.models import generate_standard_graph_input, update_model_args
+from kgcnn.layers.disjoint.attention import AttentionHeadGAT
+
+
+
+
+def make_gat(  # Input
+        input_node_shape,
+        input_edge_shape,
+        input_embedd: dict = None,
+        # Output
+        output_embedd: dict = None,
+        output_mlp: dict = None,
+        # Model specific parameter
+        depth=3,
+        attention_heads_num = 5,
+        attention_heads_concat = False,
+        attention_args: dict = None
+):
+    """
+    Generate Interaction network.
+
+    Args:
+        input_node_shape (list): Shape of node features. If shape is (None,) embedding layer is used.
+        input_edge_shape (list): Shape of edge features. If shape is (None,) embedding layer is used.
+        input_embedd (dict): Dictionary of embedding parameters used if input shape is None. Default is
+            {'input_node_vocab': 95, 'input_edge_vocab': 5, 'input_state_vocab': 100,
+            'input_node_embedd': 64, 'input_edge_embedd': 64, 'input_state_embedd': 64,
+            'input_type': 'ragged'}.
+        output_embedd (dict): Dictionary of embedding parameters of the graph network. Default is
+            {"output_mode": 'graph', "output_type": 'padded'}.
+        output_mlp (dict): Dictionary of arguments for final MLP regression or classifcation layer. Default is
+            {"use_bias": [True, True, False], "units": [25, 10, 1],
+            "activation": ['relu', 'relu', 'sigmoid']}.
+        depth (int): Number of convolution layers. Default is 3.
+        pooling_args (dict): Dictionary for message pooling arguments. Default is
+            {'is_sorted': False, 'has_unconnected': True, 'pooling_method': "segment_mean"}
+
+    Returns:
+        model (tf.keras.model): Interaction model.
+    """
+    # default values
+    model_default = {'input_embedd': {'input_node_vocab': 95, 'input_edge_vocab': 5, 'input_state_vocab': 100,
+                                      'input_node_embedd': 64, 'input_edge_embedd': 64, 'input_state_embedd': 64,
+                                      'input_type': 'ragged'},
+                     'output_embedd': {"output_mode": 'graph', "output_type": 'padded'},
+                     'output_mlp': {"use_bias": [True, True, False], "units": [25, 10, 1],
+                                    "activation": ['relu', 'relu', 'sigmoid']},
+                     'attention_args': {"units": 32, 'is_sorted': False, 'has_unconnected': True}
+                     }
+
+    # Update default values
+    input_embedd = update_model_args(model_default['input_embedd'], input_embedd)
+    output_embedd = update_model_args(model_default['output_embedd'], output_embedd)
+    output_mlp = update_model_args(model_default['output_mlp'], output_mlp)
+    attention_args = update_model_args(model_default['attention_args'], attention_args)
+
+
+    # Make input embedding, if no feature dimension
+    node_input, n, edge_input, ed, edge_index_input, _, _ = generate_standard_graph_input(input_node_shape,
+                                                                                          input_edge_shape, None,
+                                                                                          **input_embedd)
+
+    n, node_len, ed, edge_len, edi = CastRaggedToDisjoint()([n, ed, edge_index_input])
+
+    nk = ks.layers.Dense(units=attention_args["units"])(n)
+    for i in range(0, depth):
+        heads = [AttentionHeadGAT(**attention_args)([nk, node_len, ed, edge_len, edi]) for _ in range(attention_heads_num)]
+        if attention_heads_concat:
+            nk = ks.layers.Concatenate(axis=-1)(heads)
+        else:
+            nk = ks.layers.Average()(heads)
+
+    n = nk
+    if output_embedd["output_mode"] == 'graph':
+        out = PoolingNodes()([n, node_len])
+        out = MLP(**output_mlp)(out)
+        main_output = ks.layers.Flatten()(out)  # will be dense
+    else:  # node embedding
+        out = MLP(**output_mlp)(n)
+        main_output = CastValuesToRagged()([out, node_len])
+        main_output = CastRaggedToDense()(main_output)  # no ragged for distribution supported atm
+
+    model = tf.keras.models.Model(inputs=[node_input, edge_input, edge_index_input], outputs=main_output)
+
+    return model
