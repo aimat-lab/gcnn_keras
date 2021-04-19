8bad828edd1ccd6cb5c335c4a9554169855b7941
cm5828 <patrick.reiser@kit.edu>
2021-04-19 22:58:30 +0200

    refactor to ops

--- kgcnn/ops/scatter.py
+++ kgcnn/ops/scatter.py
@@ -14,7 +14,7 @@ def _scatter_segment_tensor_nd(data, segment_ids, target_shape):
     Returns:
 
     """
-    # If max id is not in segment_ids requires scatter into tensor of correct shape[0] that matches target firs dim
+    # If max id is not in segment_ids requires scatter into tensor of correct shape[0] that matches target first dim
     # out_target_shape = (tf.shape(nod, out_type=nodind.dtype)[0], ks.backend.int_shape(dens)[-1])
     out_target_shape = tf.concat([target_shape[:1], tf.shape(data)[1:]], axis=0)
     # Make segment indices
@@ -22,18 +22,3 @@ def _scatter_segment_tensor_nd(data, segment_ids, target_shape):
     out_tensor = tf.scatter_nd(ks.backend.expand_dims(segment_index, axis=-1), data, out_target_shape)
 
     return out_tensor
-
-
-@tf.function
-def _tensor_scatter_nd_mean(tensor, indices, updates):
-    out_values = tf.tensor_scatter_nd_add(tensor, indices, updates)
-    # If updates are slices, don't need to build full rank tensor to count updates
-    counts_tensor = tf.zeros(
-        tf.concat([tf.shape(tensor)[:tf.shape(indices)[-1]], tf.ones_like(tf.shape(tensor))[tf.shape(indices)[-1]:]],
-                  axis=0), dtype=tensor.dtype)
-    counts_updates = tf.ones(
-        tf.concat([tf.shape(updates)[:tf.shape(indices)[-1]], tf.ones_like(tf.shape(updates))[tf.shape(indices)[-1]:]],
-                  axis=0), dtype=updates.dtype)
-    num_values = tf.tensor_scatter_nd_add(counts_tensor, indices, counts_updates)
-    out = tf.math.divide_no_nan(out_values, num_values)
-    return out
