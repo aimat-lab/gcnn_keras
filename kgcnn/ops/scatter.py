f9411b5f947116380134bc44a2db2e26fee89e31
cm5828 <patrick.reiser@kit.edu>
2021-04-22 15:40:07 +0200

    fixed bug in ragged.pooling

--- kgcnn/ops/scatter.py
+++ kgcnn/ops/scatter.py
@@ -20,7 +20,26 @@ def _scatter_segment_tensor_nd(data, segment_ids, target_shape):
     # Make segment indices
     segment_index = tf.range(tf.shape(data)[0])
     out_tensor = tf.scatter_nd(ks.backend.expand_dims(segment_index, axis=-1), data, out_target_shape)
-
     return out_tensor
 
+
+@tf.function
+def _tensor_scatter_nd_mean(tensor, indices, updates, name=None):
+    pass
+
+
+@tf.function
+def _tensor_scatter_nd_by_name(segment_name, tensor, indices, updates, name=None):
+    pool = None
+    if segment_name in ["segment_mean", "mean", "reduce_mean"]:
+        pool = _tensor_scatter_nd_mean(tensor, indices, updates, name=name)
+    elif segment_name in ["segment_sum", "sum", "reduce_sum"]:
+        pool = tf.tensor_scatter_nd_add(tensor, indices, updates, name=name)
+    elif segment_name in ["segment_max", "max", "reduce_max"]:
+        pool = tf.tensor_scatter_nd_max(tensor, indices, updates, name=name)
+    elif segment_name in ["segment_min", "sum", "reduce_min"]:
+        pool = tf.tensor_scatter_nd_min(tensor, indices, updates, name=name)
+    else:
+        raise TypeError("Unknown pooling, choose: 'segment_mean', 'segment_sum', ...")
+    return pool
 
