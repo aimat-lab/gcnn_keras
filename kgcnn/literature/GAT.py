72f0e1a6595e82fe83077b44ad9f70f738c72c7c
cm5828 <patrick.reiser@kit.edu>
2021-07-05 21:41:11 +0200

    new simplified model format

--- kgcnn/literature/GAT.py
+++ kgcnn/literature/GAT.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import tensorflow.keras as ks
+import pprint
 
 from kgcnn.layers.attention import AttentionHeadGAT
 from kgcnn.layers.casting import ChangeTensorType
@@ -12,44 +13,17 @@ from kgcnn.utils.models import generate_node_embedding, update_model_args, gener
 # Graph Attention Networks by Veličković et al. (2018)
 # https://arxiv.org/abs/1710.10903
 
-def make_gat(  # Input
-        input_node_shape,
-        input_edge_shape,
-        input_embedding: dict = None,
-        # Output
-        output_embedding: dict = None,
-        output_mlp: dict = None,
-        # Model specific parameter
-        depth=3,
-        attention_heads_num=5,
-        attention_heads_concat=False,
-        attention_args: dict = None
-):
+
+def make_gat(**kwargs):
     """Generate Graph attention network.
 
     Args:
-        input_node_shape (list): Shape of node features. If shape is (None,) embedding layer is used.
-        input_edge_shape (list): Shape of edge features. If shape is (None,) embedding layer is used.
-        input_embedding (dict): Dictionary of embedding parameters used if input shape is None. Default is
-            {"nodes": {"input_dim": 95, "output_dim": 64},
-            "edges": {"input_dim": 5, "output_dim": 64},
-            "state": {"input_dim": 100, "output_dim": 64}}.
-        output_embedding (dict): Dictionary of embedding parameters of the graph network. Default is
-            {"output_mode": 'graph', "output_tensor_type": 'padded'}.
-        output_mlp (dict): Dictionary of arguments for final MLP regression or classification layer. Default is
-            {"use_bias": [True, True, False], "units": [25, 10, 1],
-            "activation": ['relu', 'relu', 'sigmoid']}.
-        depth (int): Number of convolution layers. Default is 3.
-        attention_heads_num (int): Number of attention heads. Default is 5.
-        attention_heads_concat (bool): Concat attention. Default is False.
-        attention_args (dict): Layer arguments for attention layer. Default is
-            {"units": 32, "use_final_activation": False, "use_edge_features": True,
-            "has_self_loops": True, "activation": "kgcnn>leaky_relu"}
+        **kwargs
 
     Returns:
         tf.keras.models.Model: GAT model.
     """
-    model_args = locals()
+    model_args = kwargs
     model_default = {'input_node_shape': [None], 'input_edge_shape': [None],
                      'input_embedding': {"nodes": {"input_dim": 95, "output_dim": 64},
                                          "edges": {"input_dim": 5, "output_dim": 64},
@@ -65,12 +39,20 @@ def make_gat(  # Input
                      'attention_heads_concat': False,
                      }
     m = update_model_args(model_default, model_args)
-    # Update default values
+    print("INFO: make_gat() kwargs:")
+    pprint.pprint(m)
+
+    # Local variables for model args
     input_embedding = m['input_embedding']
     output_embedding = m['output_embedding']
     output_mlp = m['output_mlp']
     attention_args = m['attention_args']
     pooling_nodes_args = m['pooling_nodes_args']
+    depth = m['depth']
+    input_node_shape = m['input_node_shape']
+    input_edge_shape = m['input_edge_shape']
+    attention_heads_num = m['attention_heads_num']
+    attention_heads_concat = m['attention_heads_concat']
 
     # Make input
     node_input = ks.layers.Input(shape=input_node_shape, name='node_input', dtype="float32", ragged=True)
@@ -81,6 +63,7 @@ def make_gat(  # Input
     ed = generate_edge_embedding(edge_input, input_edge_shape, input_embedding['edges'])
     edi = edge_index_input
 
+    # Model
     nk = Dense(units=attention_args["units"], activation="linear")(n)
     for i in range(0, depth):
         heads = [AttentionHeadGAT(**attention_args)([n, ed, edi]) for _ in range(attention_heads_num)]
@@ -89,8 +72,9 @@ def make_gat(  # Input
         else:
             nk = Average()(heads)
             nk = Activation(activation=attention_args["activation"])(nk)
-
     n = nk
+
+    # Output embedding choice
     if output_embedding["output_mode"] == 'graph':
         out = PoolingNodes(**pooling_nodes_args)(n)
         out = MLP(**output_mlp)(out)
--- kgcnn/literature/GATv2.py
+++ kgcnn/literature/GATv2.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import tensorflow.keras as ks
+import pprint
 
 from kgcnn.layers.attention import AttentionHeadGATV2
 from kgcnn.layers.casting import ChangeTensorType
@@ -15,39 +16,11 @@ from kgcnn.utils.models import generate_node_embedding, update_model_args, gener
 # How Attentive are Graph Attention Networks?
 # by Brody et al. (2021)
 
-def make_gat_v2(  # Input
-        input_node_shape,
-        input_edge_shape,
-        # Output
-        output_embedding: dict = None,
-        output_mlp: dict = None,
-        # Model specific parameter
-        input_embedding: dict = None,
-        depth=3,
-        attention_heads_num=5,
-        attention_heads_concat=False,
-        attention_args: dict = None
-):
+def make_gat_v2(**kwargs):
     """Generate Graph attention network.
 
     Args:
-        input_node_shape (list): Shape of node features. If shape is (None,) embedding layer is used.
-        input_edge_shape (list): Shape of edge features. If shape is (None,) embedding layer is used.
-        input_embedding (dict): Dictionary of embedding parameters used if input shape is None. Default is
-            {"nodes": {"input_dim": 95, "output_dim": 64},
-            "edges": {"input_dim": 5, "output_dim": 64},
-            "state": {"input_dim": 100, "output_dim": 64}}.
-        output_embedding (dict): Dictionary of embedding parameters of the graph network. Default is
-            {"output_mode": 'graph', "output_tensor_type": 'padded'}.
-        output_mlp (dict): Dictionary of arguments for final MLP regression or classification layer. Default is
-            {"use_bias": [True, True, False], "units": [25, 10, 1],
-            "activation": ['relu', 'relu', 'sigmoid']}.
-        depth (int): Number of convolution layers. Default is 3.
-        attention_heads_num (int): Number of attention heads. Default is 5.
-        attention_heads_concat (bool): Concat attention. Default is False.
-        attention_args (dict): Layer arguments for attention layer. Default is
-            {"units": 32, "use_final_activation": False, "use_edge_features": True,
-            "has_self_loops": True, "activation": "kgcnn>leaky_relu"}
+        **kwargs
 
     Returns:
         tf.keras.models.Model: GAT model.
@@ -68,12 +41,20 @@ def make_gat_v2(  # Input
                      'attention_heads_concat': False,
                      }
     m = update_model_args(model_default, model_args)
-    # Shortcuts for model_args
-    attention_args = m['attention_args']
-    output_embedding = m['output_embedding']
+    print("INFO: make_gat() kwargs:")
+    pprint.pprint(m)
+
+    # Local variables for model args
     input_embedding = m['input_embedding']
+    output_embedding = m['output_embedding']
     output_mlp = m['output_mlp']
+    attention_args = m['attention_args']
     pooling_nodes_args = m['pooling_nodes_args']
+    depth = m['depth']
+    input_node_shape = m['input_node_shape']
+    input_edge_shape = m['input_edge_shape']
+    attention_heads_num = m['attention_heads_num']
+    attention_heads_concat = m['attention_heads_concat']
 
     # Make input
     node_input = ks.layers.Input(shape=m['input_node_shape'], name='node_input', dtype="float32", ragged=True)
@@ -85,6 +66,7 @@ def make_gat_v2(  # Input
     ed = generate_edge_embedding(edge_input, input_edge_shape, input_embedding['edges'])
     edi = edge_index_input
 
+    # Model
     nk = Dense(units=attention_args["units"], activation="linear")(n)
     for i in range(0, depth):
         heads = [AttentionHeadGATV2(**attention_args)([nk, ed, edi]) for _ in range(attention_heads_num)]
@@ -93,8 +75,9 @@ def make_gat_v2(  # Input
         else:
             nk = Average()(heads)
             nk = Activation(activation=attention_args["activation"])(nk)
-
     n = nk
+
+    # Output embedding choice
     if output_embedding["output_mode"] == 'graph':
         out = PoolingNodes(**pooling_nodes_args)(n)
         out = MLP(**output_mlp)(out)
