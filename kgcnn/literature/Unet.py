791391ebd600aa6d2438353eaef6640fdaae6682
cm5828 <patrick.reiser@kit.edu>
2021-07-07 20:03:30 +0200

    refactor model args

--- kgcnn/literature/Unet.py
+++ kgcnn/literature/Unet.py
@@ -31,7 +31,7 @@ def make_unet(**kwargs):
                                          "state": {"input_dim": 100, "output_dim": 64}},
                      'output_embedding': {"output_mode": 'graph', "output_tensor_type": 'padded'},
                      'output_mlp': {"use_bias": [True, False], "units": [25, 1], "activation": ['relu', 'sigmoid']},
-                     'hidden_dim': 32,   'use_bias': True,
+                     'dense_args': {'hidden_dim': 32, 'use_bias': True, 'activation': 'linear'},
                      'top_k_args': {'k': 0.3, 'kernel_initializer': 'ones'},
                      'activation': 'relu',
                      'use_reconnect': True,
@@ -56,6 +56,8 @@ def make_unet(**kwargs):
     top_k_args = m['top_k_args']
     depth = m['depth']
     use_reconnect = m['use_reconnect']
+    dense_args = m['dense_args']
+    activation = m['activation']
 
     # Make input
     node_input = ks.layers.Input(shape=input_node_shape, name='node_input', dtype="float32", ragged=True)
@@ -68,7 +70,7 @@ def make_unet(**kwargs):
     edi = edge_index_input
 
     # Model
-    n = Dense(hidden_dim, use_bias=use_bias, activation='linear')(n)
+    n = Dense(**dense_args)(n)
     in_graph = [n, ed, edi]
     graph_list = [in_graph]
     map_list = []
@@ -80,7 +82,7 @@ def make_unet(**kwargs):
         n, ed, edi = i_graph
         # GCN layer
         eu = GatherNodesOutgoing(**gather_args)([n, edi])
-        eu = Dense(hidden_dim, use_bias=use_bias, activation='linear')(eu)
+        eu = Dense(**dense_args)(eu)
         nu = PoolingLocalEdges(**pooling_args)([n, eu, edi])  # Summing for each node connection
         n = Activation(activation=activation)(nu)
 
@@ -105,7 +107,7 @@ def make_unet(**kwargs):
         n = Add()([n, o_graph[0]])
         # GCN
         eu = GatherNodesOutgoing(**gather_args)([n, edi])
-        eu = Dense(hidden_dim, use_bias=use_bias, activation='linear')(eu)
+        eu = Dense(**dense_args)(eu)
         nu = PoolingLocalEdges(**pooling_args)([n, eu, edi])  # Summing for each node connection
         n = Activation(activation=activation)(nu)
 
