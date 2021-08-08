e9f968a36bddc56849f4b5b41b152ac39a1f1d4f
cm5828 <patrick.reiser@kit.edu>
2021-08-08 22:39:46 +0200

    rework dataset and training

--- kgcnn/literature/AttentiveFP.py
+++ kgcnn/literature/AttentiveFP.py
@@ -7,7 +7,7 @@ from kgcnn.layers.casting import ChangeTensorType
 from kgcnn.layers.keras import Dense, Dropout
 from kgcnn.layers.conv.mpnn_conv import GRUUpdate
 from kgcnn.layers.mlp import MLP
-from kgcnn.utils.models import generate_node_embedding, update_model_kwargs_logic, generate_edge_embedding
+from kgcnn.utils.models import generate_embedding, update_model_kwargs
 import kgcnn.ops.activ
 
 # Pushing the Boundaries of Molecular Representation for Drug Discovery with the Graph Attention Mechanism
@@ -16,44 +16,32 @@ import kgcnn.ops.activ
 # Publication Date:August 13, 2019
 # https://doi.org/10.1021/acs.jmedchem.9b00959
 
+model_default = {'name': "AttentiveFP",
+                 'inputs': [{'shape': (None,), 'name': "node_attributes", 'dtype': 'float32', 'ragged': True},
+                            {'shape': (None,), 'name': "edge_attributes", 'dtype': 'float32', 'ragged': True},
+                            {'shape': (None, 2), 'name': "edge_indices", 'dtype': 'int64', 'ragged': True}],
+                 'input_embedding': {"node": {"input_dim": 95, "output_dim": 64},
+                                     "edge": {"input_dim": 5, "output_dim": 64}},
+                 'output_embedding': 'graph',
+                 'output_mlp': {"use_bias": [True, True, False], "units": [25, 10, 1],
+                                "activation": ['relu', 'relu', 'sigmoid']},
+                 'attention_args': {"units": 32},
+                 'depth': 3,
+                 'dropout': 0.1,
+                 'verbose': 1
+                 }
 
-def make_model(**kwargs):
-    """Make AttentiveFP network.
 
-    Args:
-        **kwargs
-
-    Returns:
-        tf.keras.models.Model: AttentiveFP model.
-    """
-    model_args = kwargs
-    model_default = {'name': "AttentiveFP",
-                     'inputs': [{'shape': (None,), 'name': "node_attributes", 'dtype': 'float32', 'ragged': True},
-                                {'shape': (None,), 'name': "edge_attributes", 'dtype': 'float32', 'ragged': True},
-                                {'shape': (None, 2), 'name': "edge_indices", 'dtype': 'int64', 'ragged': True}],
-                     'input_embedding': {"node_attributes": {"input_dim": 95, "output_dim": 64},
-                                         "edge_attributes": {"input_dim": 5, "output_dim": 64}},
-                     'output_embedding': 'graph',
-                     'output_mlp': {"use_bias": [True, True, False], "units": [25, 10, 1],
-                                    "activation": ['relu', 'relu', 'sigmoid']},
-                     'attention_args': {"units": 32},
-                     'depth': 3,
-                     'dropout': 0.1,
-                     'verbose': 1
-                     }
-    m = update_model_kwargs_logic(model_default, model_args)
-    if m['verbose'] > 0:
-        print("INFO:kgcnn: Updated functional make model kwargs:")
-        pprint.pprint(m)
-
-    # Local variables for model args
-    inputs = m['inputs']
-    depth = m['depth']
-    dropout = m['dropout']
-    input_embedding = m['input_embedding']
-    output_embedding = m['output_embedding']
-    output_mlp = m['output_mlp']
-    attention_args = m['attention_args']
+@update_model_kwargs(model_default)
+def make_model(inputs=None,
+               depth=None,
+               dropout=None,
+               input_embedding=None,
+               output_embedding=None,
+               output_mlp=None,
+               attention_args=None,
+               **kwargs):
+    """Make AttentiveFP network."""
 
     # Make input
     node_input = ks.layers.Input(**inputs[0])
@@ -61,8 +49,8 @@ def make_model(**kwargs):
     edge_index_input = ks.layers.Input(**inputs[2])
 
     # Embedding, if no feature dimension
-    n = generate_node_embedding(node_input, inputs[0]['shape'], input_embedding[inputs[0]['name']])
-    ed = generate_edge_embedding(edge_input, inputs[1]['shape'], input_embedding[inputs[1]['name']])
+    n = generate_embedding(node_input, inputs[0]['shape'], input_embedding['node'])
+    ed = generate_embedding(edge_input, inputs[1]['shape'], input_embedding['edge'])
     edi = edge_index_input
 
     # Model
