6f92f99945129358234edf2226f56aeb9db21ebf
cm5828 <patrick.reiser@kit.edu>
2021-08-08 22:36:05 +0200

    rework dataset and training

--- kgcnn/literature/GATv2.py
+++ kgcnn/literature/GATv2.py
@@ -7,8 +7,7 @@ from kgcnn.layers.casting import ChangeTensorType
 from kgcnn.layers.keras import Concatenate, Dense, Average, Activation
 from kgcnn.layers.mlp import MLP
 from kgcnn.layers.pool.pooling import PoolingNodes
-from kgcnn.utils.models import generate_node_embedding, update_model_kwargs_logic, generate_edge_embedding
-
+from kgcnn.utils.models import generate_embedding, update_model_kwargs
 
 # Graph Attention Networks by Veličković et al. (2018)
 # https://arxiv.org/abs/1710.10903
@@ -16,46 +15,35 @@ from kgcnn.utils.models import generate_node_embedding, update_model_kwargs_logi
 # How Attentive are Graph Attention Networks?
 # by Brody et al. (2021)
 
-def make_model(**kwargs):
-    """Generate Graph attention network.
+model_default = {'name': "GATv2",
+                 'inputs': [{'shape': (None,), 'name': "node_attributes", 'dtype': 'float32', 'ragged': True},
+                            {'shape': (None,), 'name': "edge_attributes", 'dtype': 'float32', 'ragged': True},
+                            {'shape': (None, 2), 'name': "edge_indices", 'dtype': 'int64', 'ragged': True}],
+                 'input_embedding': {"node": {"input_dim": 95, "output_dim": 64},
+                                     "edge": {"input_dim": 5, "output_dim": 64}},
+                 'output_embedding': 'graph',
+                 'output_mlp': {"use_bias": [True, True, False], "units": [25, 10, 1],
+                                "activation": ['relu', 'relu', 'sigmoid']},
+                 'attention_args': {"units": 32, "use_final_activation": False, "use_edge_features": True,
+                                    "has_self_loops": True, "activation": "kgcnn>leaky_relu", "use_bias": True},
+                 'pooling_nodes_args': {'pooling_method': 'mean'},
+                 'depth': 3, 'attention_heads_num': 5,
+                 'attention_heads_concat': False, 'verbose': 1
+                 }
 
-    Args:
-        **kwargs
 
-    Returns:
-        tf.keras.models.Model: GAT model.
-    """
-    model_args = kwargs
-    model_default = {'name': "GATv2",
-                     'inputs': [{'shape': (None,), 'name': "node_attributes", 'dtype': 'float32', 'ragged': True},
-                                {'shape': (None,), 'name': "edge_attributes", 'dtype': 'float32', 'ragged': True},
-                                {'shape': (None, 2), 'name': "edge_indices", 'dtype': 'int64', 'ragged': True}],
-                     'input_embedding': {"node_attributes": {"input_dim": 95, "output_dim": 64},
-                                         "edge_attributes": {"input_dim": 5, "output_dim": 64}},
-                     'output_embedding': 'graph',
-                     'output_mlp': {"use_bias": [True, True, False], "units": [25, 10, 1],
-                                    "activation": ['relu', 'relu', 'sigmoid']},
-                     'attention_args': {"units": 32, "use_final_activation": False, "use_edge_features": True,
-                                        "has_self_loops": True, "activation": "kgcnn>leaky_relu", "use_bias": True},
-                     'pooling_nodes_args': {'pooling_method': 'mean'},
-                     'depth': 3, 'attention_heads_num': 5,
-                     'attention_heads_concat': False, 'verbose': 1
-                     }
-    m = update_model_kwargs_logic(model_default, model_args)
-    if m['verbose'] > 0:
-        print("INFO:kgcnn: Updated functional make model kwargs:")
-        pprint.pprint(m)
-
-    # Local variables for model args
-    inputs = m['inputs']
-    input_embedding = m['input_embedding']
-    output_embedding = m['output_embedding']
-    output_mlp = m['output_mlp']
-    attention_args = m['attention_args']
-    pooling_nodes_args = m['pooling_nodes_args']
-    depth = m['depth']
-    attention_heads_num = m['attention_heads_num']
-    attention_heads_concat = m['attention_heads_concat']
+@update_model_kwargs(model_default)
+def make_model(inputs=None,
+               input_embedding=None,
+               output_embedding=None,
+               output_mlp=None,
+               attention_args=None,
+               pooling_nodes_args=None,
+               depth=None,
+               attention_heads_num=None,
+               attention_heads_concat=None,
+               **kwargs):
+    """Generate Graph attention network."""
 
     # Make input
     node_input = ks.layers.Input(**inputs[0])
@@ -63,8 +51,8 @@ def make_model(**kwargs):
     edge_index_input = ks.layers.Input(**inputs[2])
 
     # Embedding, if no feature dimension
-    n = generate_node_embedding(node_input, inputs[0]['shape'], input_embedding[inputs[0]['name']])
-    ed = generate_edge_embedding(edge_input, inputs[1]['shape'], input_embedding[inputs[1]['name']])
+    n = generate_embedding(node_input, inputs[0]['shape'], input_embedding['node'])
+    ed = generate_embedding(edge_input, inputs[1]['shape'], input_embedding['edge'])
     edi = edge_index_input
 
     # Model
