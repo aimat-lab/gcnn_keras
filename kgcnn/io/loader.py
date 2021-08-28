85b00f4f85543ab8d5e338a647baff5eb871b9dd
cm5828 <patrick.reiser@kit.edu>
2021-08-28 19:41:54 +0200

    refactor geometric utility and add docs

--- kgcnn/io/loader.py
+++ kgcnn/io/loader.py
@@ -5,14 +5,37 @@ from kgcnn.utils.data import ragged_tensor_from_nested_numpy
 
 
 class NumpyTensorList:
+    """An utility class to keep lists of numpy arrays. Multiple lists are handled in parallel and therefore should
+    have the same size. The behavior should be similar to :obj:`train_test_split` from scikit-learn.
+    The length of each list should match the length of the dataset and
+    each list-item be a numpy-tensor for a data-point. The lists could be X, y or different graph attributes.
 
+    """
 
     def __init__(self, *args):
         self._tensor_list = [x for x in args]
+        if len(self._tensor_list) <= 0:
+            print("WARNING:kgcnn: Received empty list input for `NumpyTensorList`. \
+                Expected one or more list of numpy arrays.")
+
+        # check that all have same length
         self._data_length = None
-        # check that all
+        length_test = np.array([len(x) for x in self._tensor_list], dtype="int")
+        if len(length_test) > 0:
+            if np.all(length_test == length_test[0]):
+                self._data_length = length_test[0]
+            else:
+                print("WARNING:kgcnn: Length of list input to `NumpyTensorList` are different.")
 
     def __getitem__(self, item):
+        """Indexing or getitem method to apply to each list and return a new :obj:`NumpyTensorList`.
+
+        Args:
+            item (list, int, np.ndarray): Index or list of indices to collect from each list in self.
+
+        Returns:
+            NumpyTensorList: New :obj:`NumpyTensorList` with only `items` in each list.
+        """
         if isinstance(item, int):
             return NumpyTensorList(*[[x[item]] for x in self._tensor_list])
         elif isinstance(item, slice):
@@ -20,11 +43,11 @@ class NumpyTensorList:
         elif hasattr(item, "__getitem__"):
             return NumpyTensorList(*[[x[y] for y in item] for x in self._tensor_list])
         else:
-            raise ValueError("Can not iterate over input of getitem() for `NumpyTensorList`: ", item)
+            raise ValueError("ERROR:kgcnn: Can not iterate over input of getitem() for `NumpyTensorList`: ", item)
 
     def tensor(self, ragged=None):
         if ragged is None:
-            ragged = [False]*len(self._tensor_list)
+            ragged = [False] * len(self._tensor_list)
         assert len(ragged) == len(self._tensor_list)
         out_list = []
         for i, x in enumerate(self._tensor_list):
@@ -34,7 +57,15 @@ class NumpyTensorList:
                 out_list.append(ragged_tensor_from_nested_numpy(x))
         return out_list
 
-    def pop(self, index):
+    def pop(self, index: int):
+        """Remove a single item at index from each list referenced by self.
+
+        Args:
+            index (int): Index of item to remove.
+
+        Returns:
+            NumpyTensorList: New :obj:`NumpyTensorList` with item at `index` removed in each list.
+        """
         for i, x in enumerate(self._tensor_list):
             if isinstance(x, list):
                 x.pop(index)
@@ -42,7 +73,7 @@ class NumpyTensorList:
                 self._tensor_list[i] = [x[j] for j in range(len(x)) if j != index]
 
     def __len__(self):
+        """Get length of each list."""
         return len(self._tensor_list[0])
-
 
 # test = NumpyTensorList([np.array([1, 2])], [np.array([1, 2])])
