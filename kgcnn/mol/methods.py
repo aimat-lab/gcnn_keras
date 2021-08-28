39158d287cc53f0b36bf33c2f1b860348205cbab
cm5828 <patrick.reiser@kit.edu>
2021-08-28 17:47:31 +0200

    refactor geometric utility and add docs

--- kgcnn/mol/methods.py
+++ kgcnn/mol/methods.py
@@ -28,7 +28,7 @@ def get_connectivity_from_inverse_distance_matrix(inv_dist_mat, protons, radii_d
         np.ndarray: Connectivity table with 1 for chemical bond and zero otherwise of shape `(..., N, N)`.
     """
     # Dictionary of bond radii
-    proton_raddi_dict = np.array(
+    proton_radii_dict = np.array(
         [0, 0.34, 0.46, 1.2, 0.94, 0.77, 0.75, 0.71, 0.63, 0.64, 0.67, 1.4, 1.25, 1.13, 1.04, 1.1, 1.02, 0.99, 0.96,
          1.76, 1.54, 1.33, 1.22, 1.21, 1.1, 1.07, 1.04, 1.0, 0.99, 1.01, 1.09, 1.12, 1.09, 1.15, 1.1, 1.14, 1.17, 1.89,
          1.67, 1.47, 1.39, 1.32, 1.24, 1.15, 1.13, 1.13, 1.19, 1.15, 1.23, 1.28, 1.26, 1.26, 1.23, 1.32, 1.31, 2.09,
@@ -36,7 +36,7 @@ def get_connectivity_from_inverse_distance_matrix(inv_dist_mat, protons, radii_d
          1.23, 1.18, 1.16, 1.11, 1.12, 1.13, 1.32, 1.3, 1.3, 1.36, 1.31, 1.38, 1.42, 2.01, 1.81, 1.67, 1.58, 1.52, 1.53,
          1.54, 1.55])
     if radii_dict is None:
-        radii_dict = proton_raddi_dict  # index matches atom number
+        radii_dict = proton_radii_dict  # index matches atom number
     # Get Radii
     protons = np.array(protons, dtype=np.int)
     radii = radii_dict[protons]
@@ -44,17 +44,17 @@ def get_connectivity_from_inverse_distance_matrix(inv_dist_mat, protons, radii_d
     shape_rad = radii.shape
     r1 = np.expand_dims(radii, axis=len(shape_rad) - 1)
     r2 = np.expand_dims(radii, axis=len(shape_rad))
-    rmat = r1 + r2
-    rmat = k2 * rmat
-    rr = rmat * inv_dist_mat
+    r_mat = r1 + r2
+    r_mat = k2 * r_mat
+    rr = r_mat * inv_dist_mat
     damp = (1.0 + np.exp(-k1 * (rr - 1.0)))
     damp = 1.0 / damp
     if force_bonds:  # Have at least one bond
-        maxvals = np.expand_dims(np.argmax(damp, axis=-1), axis=-1)
-        np.put_along_axis(damp, maxvals, 1, axis=-1)
+        max_vals = np.expand_dims(np.argmax(damp, axis=-1), axis=-1)
+        np.put_along_axis(damp, max_vals, 1, axis=-1)
         # To make it symmetric transpose last two axis
         damp = np.swapaxes(damp, -2, -1)
-        np.put_along_axis(damp, maxvals, 1, axis=-1)
+        np.put_along_axis(damp, max_vals, 1, axis=-1)
         damp = np.swapaxes(damp, -2, -1)
     damp[damp < cutoff] = 0
     bond_tab = np.round(damp)
