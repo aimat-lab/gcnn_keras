2e2ec1d1b8f9eb3eaac1b80ea26585022eca0136
cm5828 <patrick.reiser@kit.edu>
2021-10-21 17:29:26 +0200

    updated test, which were for very old verison

--- test/test_conv_attention.py
+++ test/test_conv_attention.py
@@ -29,34 +29,25 @@ class TestAttentionDisjoint(unittest.TestCase):
 
     def test_attention_pooling(self):
 
-        result = PoolingLocalEdgesAttention(input_tensor_type="values_partition")\
-                                                ([[np.array([1.0,1.0]), np.array([1,1])],
-                                               [np.array([[100.0],[0.0],[100.0],[0.0]]),np.array([2,2])],
-                                               [np.array([[0.0],[1.0],[0.0],[1.0]]), np.array([2,2])],
-                                               [np.array([[0,1],[0,0],[1,0],[1,0]]),np.array([2,2]) ]
-                                               ])
+        layer = PoolingLocalEdgesAttention()
+        result = layer([tf.RaggedTensor.from_row_lengths(np.array([[1.0],[1.0]]), np.array([1,1])),
+                       tf.RaggedTensor.from_row_lengths(np.array([[100.0],[0.0],[100.0],[0.0]]),np.array([2,2])),
+                       tf.RaggedTensor.from_row_lengths(np.array([[0.0],[1.0],[0.0],[1.0]]), np.array([2,2])),
+                       tf.RaggedTensor.from_row_lengths(np.array([[0,1],[0,0],[1,0],[1,0]]),np.array([2,2]))]
+                       )
         result = result[0].numpy()
         self.assertTrue(np.abs(result[0] - 100.0 * 1/(np.exp(1)+1) ) < 1e-4)
 
     def test_attention_head(self):
 
-        node = tf.ragged.constant(self.n1, ragged_rank=1, inner_shape=(1,))
-        edgeind = tf.ragged.constant(self.ei1, ragged_rank=1, inner_shape=(2,))
-        edgefeat = tf.ragged.constant(self.e1, ragged_rank=1, inner_shape=(1,))
+        n = tf.ragged.constant(self.n1, ragged_rank=1, inner_shape=(1,))
+        edi = tf.ragged.constant(self.ei1, ragged_rank=1, inner_shape=(2,))
+        ed = tf.ragged.constant(self.e1, ragged_rank=1, inner_shape=(1,))
 
-        node_indexing = 'sample'
-        partition_type = 'row_length'
-        tens_type = "values_partition"
-        n = ChangeTensorType(input_tensor_type="ragged", output_tensor_type=tens_type)(node)
-        ed = ChangeTensorType(input_tensor_type="ragged", output_tensor_type=tens_type)(edgefeat)
-        edi = ChangeTensorType(input_tensor_type="ragged", output_tensor_type=tens_type)(edgeind)
-        edi = ChangeIndexing(input_tensor_type=tens_type, to_indexing=node_indexing)([n, edi])
-
-
-        layer = AttentionHeadGAT(5,node_indexing=node_indexing, input_tensor_type=tens_type,
-                                 partition_type=partition_type)
+        layer = AttentionHeadGAT(5)
         result = layer([n, ed, edi])
-        self.assertTrue(np.all(np.array(result[0].shape) == np.array([23,5])))
+
+        self.assertTrue(np.all(np.array(result[0].shape) == np.array([8,5])))
         # layer.get_config()
 
 
--- test/test_pool_pooling.py
+++ test/test_pool_pooling.py
@@ -29,27 +29,15 @@ class TestPoolingLocalEdgesLSTM(unittest.TestCase):
            [0.35355339059327373]]]
 
     def test_pooling_LSTM(self):
-        node = tf.ragged.constant(self.n1, ragged_rank=1, inner_shape=(1,))
-        edgeind = tf.ragged.constant(self.ei1, ragged_rank=1, inner_shape=(2,))
-        edgefeat = tf.ragged.constant(self.e1, ragged_rank=1, inner_shape=(1,))
+        n = tf.ragged.constant(self.n1, ragged_rank=1, inner_shape=(1,))
+        edi = tf.ragged.constant(self.ei1, ragged_rank=1, inner_shape=(2,))
+        ed = tf.ragged.constant(self.e1, ragged_rank=1, inner_shape=(1,))
 
-
-        node_indexing = 'sample'
-        partition_type = 'row_length'
-        tens_type = "values_partition"
-        n = ChangeTensorType(input_tensor_type="ragged", output_tensor_type=tens_type)(node)
-        ed = ChangeTensorType(input_tensor_type="ragged", output_tensor_type=tens_type)(edgefeat)
-        edi = ChangeTensorType(input_tensor_type="ragged", output_tensor_type=tens_type)(edgeind)
-        edi = ChangeIndexing(input_tensor_type=tens_type, to_indexing=node_indexing)([n, edi])
-
-
-
-        ns = GatherNodes(input_tensor_type=tens_type, node_indexing=node_indexing,
-                         partition_type=partition_type)([n, edi])
-        messages = Concatenate(axis=-1, input_tensor_type=tens_type)([ed,ns])
-        out = PoolingLocalEdgesLSTM(units=3, input_tensor_type=tens_type,
-                                    node_indexing=node_indexing)([n, messages, edi])
-        self.assertTrue(np.all(np.array(out[0].shape) == np.array([23,3])))
+        ns = GatherNodes()([n, edi])
+        messages = Concatenate(axis=-1)([ed,ns])
+        out = PoolingLocalEdgesLSTM(units=3)([n, messages, edi])
+        print(out[0].shape)
+        self.assertTrue(np.all(np.array(out[0].shape) == np.array([8,3])))
 
 
 if __name__ == '__main__':
