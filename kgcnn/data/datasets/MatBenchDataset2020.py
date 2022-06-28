4ee8d12c087fc43a2d439d14790fc41004691f21
PatReis <pat.reiser@web.de>
2022-06-28 14:23:03 +0200

    Update docs.

--- kgcnn/data/datasets/MatBenchDataset2020.py
+++ kgcnn/data/datasets/MatBenchDataset2020.py
@@ -2,9 +2,10 @@ import os
 
 from kgcnn.data.crystal import CrystalDataset
 from kgcnn.data.download import DownloadDataset
+from kgcnn.data.utils import load_json_file
 
 
-class MoleculeNetDataset2018(CrystalDataset, DownloadDataset):
+class MatBenchDataset2020(CrystalDataset, DownloadDataset):
     """Downloader for DeepChem MoleculeNetDataset 2018 class.
 
     """
@@ -12,20 +13,26 @@ class MoleculeNetDataset2018(CrystalDataset, DownloadDataset):
         "matbench_steels": {"dataset_name": "matbench_steels",
                             "download_file_name": 'matbench_steels.json.gz',
                             "data_directory_name": "matbench_steels", "extract_gz": True,
-                            "extract_file_name": 'matbench_steels.pymatgen.json'},
+                            "extract_file_name": 'matbench_steels.json'},
         "matbench_jdft2d": {"dataset_name": "matbench_jdft2d",
                             "download_file_name": 'matbench_jdft2d.json.gz',
                             "data_directory_name": "matbench_jdft2d", "extract_gz": True,
-                            "extract_file_name": 'matbench_jdft2d.pymatgen.json'},
+                            "extract_file_name": 'matbench_jdft2d.json'},
         "matbench_phonons": {"dataset_name": "matbench_phonons",
                             "download_file_name": 'matbench_phonons.json.gz',
                             "data_directory_name": "matbench_phonons", "extract_gz": True,
-                            "extract_file_name": 'matbench_phonons.pymatgen.json'},
+                            "extract_file_name": 'matbench_phonons.json'},
 
     }
     datasets_prepare_data_info = {
+        "matbench_steels": {},
+        "matbench_jdft2d": {},
+        "matbench_phonons": {}
     }
     datasets_read_in_memory_info = {
+        "matbench_steels": {},
+        "matbench_jdft2d": {},
+        "matbench_phonons": {}
     }
 
     def __init__(self, dataset_name: str, reload: bool = False, verbose: int = 1):
@@ -42,8 +49,8 @@ class MoleculeNetDataset2018(CrystalDataset, DownloadDataset):
         CrystalDataset.__init__(self, verbose=verbose, dataset_name=dataset_name)
 
         # Prepare download
-        if dataset_name in self.datsets_download_info:
-            self.download_info = self.datsets_download_info[dataset_name]
+        if dataset_name in self.datasets_download_info:
+            self.download_info = self.datasets_download_info[dataset_name]
             self.download_info.update({"download_url": "https://ml.materialsproject.org/projects/" +
                                                        self.download_info["download_file_name"]})
         else:
@@ -57,12 +64,16 @@ class MoleculeNetDataset2018(CrystalDataset, DownloadDataset):
         self.file_name = self.download_file_name if self.extract_file_name is None else self.extract_file_name
         self.dataset_name = dataset_name
         self.require_prepare_data = True
-        self.fits_in_memory = True
+        self.fits_in_memory = False
 
         if self.require_prepare_data:
             self.prepare_data(overwrite=reload, **self.datasets_prepare_data_info[self.dataset_name])
         if self.fits_in_memory:
             self.read_in_memory(**self.datasets_read_in_memory_info[self.dataset_name])
 
+    def prepare_data(self, cif_column_name: str = None, overwrite: bool = False):
+        self.data = load_json_file(self.file_path)
 
-data = MoleculeNetDataset2018("matbench_steels")
+
+
+data = MatBenchDataset2020("matbench_steels")
