7054a5b8e93f37f9b106c27b49b8cfdfdadbc13f
Robin Ruff <robin.ruff.98@googlemail.com>
2022-08-10 17:21:05 +0200

    Refined CGCNN model, added crystal graph builder and preprocessor classes.

--- kgcnn/layers/conv/cgcnn.py
+++ kgcnn/layers/conv/cgcnn.py
@@ -4,21 +4,34 @@ from kgcnn.layers.norm import GraphBatchNormalization
 import tensorflow.keras as ks
 import tensorflow as tf
 
+@tf.keras.utils.register_keras_serializable(package='kgcnn', name='CGCNNLayer')
 class CGCNNLayer(MessagePassingBase):
+    r"""Message Passing Layer used in the Crystal Graph Convolutional Neural Network (CGCNN).
+
+    For more information look at the paper (<https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.120.145301>)
+    or the original code base in pytorch (<https://github.com/txie-93/cgcnn>).
+
+    Args:
+        units (int): Units for Dense layer.
+        activation_s (str): Tensorflow activation applied before gating the message.
+        activation_out (str): Tensorflow activation applied the very end of the layer (after gating).
+        batch_normalization (bool): Whether to use batch normalization (GraphBatchNormalization) or not.
+    """
 
     def __init__(self, units=64, activation_s='relu', activation_out='relu', batch_normalization=False, **kwargs):
-        """Initialize MessagePassingBase layer."""
         super(CGCNNLayer, self).__init__(**kwargs)
         self.units = units
-        self.activation_f = ks.activations.sigmoid
-        self.activation_s = ks.layers.Activation(activation_s)
-        self.activation_out = ks.layers.Activation(activation_out)
+        self.activation_s = activation_s
+        self.activation_out = activation_out
+        self.batch_normalization = batch_normalization
+        self.activation_f_layer = ks.activations.sigmoid
+        self.activation_s_layer = ks.layers.Activation(activation_s)
+        self.activation_out_layer = ks.layers.Activation(activation_out)
         self.batch_norm_f = GraphBatchNormalization()
         self.batch_norm_s = GraphBatchNormalization()
         self.batch_norm_out = GraphBatchNormalization()
         self.f = ks.layers.Dense(self.units)
         self.s = ks.layers.Dense(self.units)
-        self.batch_normalization = batch_normalization
 
     def message_function(self, inputs, **kwargs):
 
@@ -30,13 +43,21 @@ class CGCNNLayer(MessagePassingBase):
         x_s, x_f = self.s(x), self.f(x)
         if self.batch_normalization:
             x_s, x_f = self.batch_norm_s(x_s), self.batch_norm_f(x_f)
-        x_s, x_f = self.activation_s(x_s), self.activation_f(x_f)
+        x_s, x_f = self.activation_s_layer(x_s), self.activation_f_layer(x_f)
         x_out = x_s * x_f # shape: (batch_size, M, self.units)
         if self.batch_normalization:
             x_out = self.batch_norm_out(x_out)
+        x_out = self.activation_out_layer(x_out)
         return x_out
 
     def update_nodes(self, inputs, **kwargs):
         nodes = inputs[0]
         nodes_update = inputs[1]
         return nodes + nodes_update
+
+    def get_config(self):
+        """Update layer config."""
+        config = super(CGCNNLayer, self).get_config()
+        config.update({"units": self.units, "activation_s": self.activation_s, "activation_out": self.activation_out,
+            "batch_normalization": self.batch_normalization})
+        return config
--- kgcnn/literature/CGCNN.py
+++ kgcnn/literature/CGCNN.py
@@ -5,6 +5,7 @@ from kgcnn.layers.geom import DisplacementVectorsASU, DisplacementVectorsUnitCel
 from kgcnn.layers.pooling import PoolingNodes, PoolingWeightedNodes
 from kgcnn.layers.modules import OptionalInputEmbedding
 from kgcnn.layers.mlp import MLP
+from kgcnn.utils.models import update_model_kwargs
 
 model_default = {'name': 'CGCNN',
                  'inputs': {'atom_attributes':
@@ -20,27 +21,31 @@ model_default = {'name': 'CGCNN',
                             'lattice_matrix':
                                 {'shape': (3, 3), 'name': 'lattice_matrix', 'dtype': 'float64'},
                             'edge_indices':
-                               {'shape': (None, 2), 'name': 'edge_indices', 'dtype': 'int64', 'ragged': True}},
-                 'input_embedding': {'node': {'input_dim': 95, 'output_dim': 128},
-                                     'edge': {'input_dim': 5, 'output_dim': 64}},
-                 'representation': 'none', # None, 'asu'
+                               {'shape': (None, 2), 'name': 'edge_indices', 'dtype': 'int64', 'ragged': True},
+                            'edge_distances':
+                               {'shape': (None,), 'name': 'edge_distances', 'dtype': 'float64', 'ragged': True}},
+                 'input_embedding': {'node': {'input_dim': 95, 'output_dim': 64}},
+                 'representation': 'unit', # None, 'asu' or 'unit'
+                 'make_distances': True,
                  'conv_layer_args': {
-                        'units': 128,
-                        'activation_s': 'relu',
-                        'activation_out': 'relu',
-                        'batch_normalization': False,
+                        'units': 64,
+                        'activation_s': 'softplus',
+                        'activation_out': 'softplus',
+                        'batch_normalization': True,
                      },
                  'expand_distance': True,
-                 'gauss_args': {'bins': 20, 'distance': 4, 'offset': 0.0, 'sigma': 0.4},
-                 'node_pooling_args': {'pooling_method': 'sum'},
+                 'gauss_args': {'bins': 40, 'distance': 8, 'offset': 0.0, 'sigma': 0.4},
+                 'node_pooling_args': {'pooling_method': 'mean'},
                  'depth': 3,
-                 'output_mlp': {'use_bias': [True, True, False], 'units': [64, 64, 1],
-                                'activation': ['selu', 'selu', 'linear']},
+                 'output_mlp': {'use_bias': [True, False], 'units': [64, 1],
+                                'activation': ['softplus', 'linear']},
                  }
 
 
+@update_model_kwargs(model_default)
 def make_model(inputs,
         representation,
+        make_distances,
         input_embedding,
         conv_layer_args,
         expand_distance,
@@ -56,46 +61,49 @@ def make_model(inputs,
     assert 'edge_indices' in inputs.keys()
     
     atom_attributes = ks.layers.Input(**inputs['atom_attributes'])
-    frac_coords = ks.layers.Input(**inputs['frac_coords'])
-    lattice_matrix = ks.layers.Input(**inputs['lattice_matrix'])
     edge_indices = ks.layers.Input(**inputs['edge_indices'])
     
-    
-    if representation == 'unit':
-        assert 'cell_translations' in inputs.keys()
-        cell_translations = ks.layers.Input(**inputs['cell_translations'])
+    if make_distances:
         
-        displacement_vectors = DisplacementVectorsUnitCell()([frac_coords, edge_indices, cell_translations])
-    elif representation == 'asu':
-        assert 'cell_translations' in inputs.keys()
-        cell_translations = ks.layers.Input(**inputs['cell_translations'])
-        assert 'multiplicities' in inputs.keys()
-        multiplicities_inp = ks.layers.Input(**inputs['multiplicities'])
-        multiplicities = tf.cast(tf.expand_dims(multiplicities_inp, -1), 'float32')
-        assert 'symmops' in inputs.keys()
-        symmops = ks.layers.Input(**inputs['symmops'])
+        frac_coords = ks.layers.Input(**inputs['frac_coords'])
+        lattice_matrix = ks.layers.Input(**inputs['lattice_matrix'])
+
+        if representation == 'unit':
+            assert 'cell_translations' in inputs.keys()
+            cell_translations = ks.layers.Input(**inputs['cell_translations'])
+            
+            displacement_vectors = DisplacementVectorsUnitCell()([frac_coords, edge_indices, cell_translations])
+        elif representation == 'asu':
+            assert 'cell_translations' in inputs.keys()
+            cell_translations = ks.layers.Input(**inputs['cell_translations'])
+            assert 'multiplicities' in inputs.keys()
+            multiplicities_inp = ks.layers.Input(**inputs['multiplicities'])
+            multiplicities = tf.cast(tf.expand_dims(multiplicities_inp, -1), 'float32')
+            assert 'symmops' in inputs.keys()
+            symmops = ks.layers.Input(**inputs['symmops'])
+            
+            displacement_vectors = DisplacementVectorsASU()([frac_coords, edge_indices, symmops, cell_translations])
+        else:
+            x_in, x_out = NodePosition()([frac_coords, edge_indices])
+            displacement_vectors = x_out - x_in
+            
+        displacement_vectors = FracToRealCoords()([displacement_vectors, lattice_matrix])
         
-        displacement_vectors = DisplacementVectorsASU()([frac_coords, edge_indices, symmops, cell_translations])
+        edge_distances = EuclideanNorm(axis=2, keepdims=True)(displacement_vectors)
+        
     else:
-        x_in, x_out = NodePosition()([frac_coords, edge_indices])
-        displacement_vectors = x_out - x_in
-        
+        edge_distances_input = ks.layers.Input(**inputs['edge_distances'])
+        edge_distances = tf.expand_dims(edge_distances_input, axis=-1)
+
+    if expand_distance:
+        edge_distances = GaussBasisLayer(**gauss_args)(edge_distances)
 
     # embedding, if no feature dimension
     n = OptionalInputEmbedding(**input_embedding['node'],
                                 use_embedding=len(inputs['atom_attributes']['shape']) < 2)(atom_attributes)
 
-    displacement_vectors = FracToRealCoords()([displacement_vectors, lattice_matrix])
-    
-    distance = EuclideanNorm(axis=2, keepdims=True)(displacement_vectors)
-    
-    if expand_distance:
-        ed = GaussBasisLayer(**gauss_args)(distance)
-    else:
-        ed = displacement_vectors
-
     for _ in range(depth):
-        n = CGCNNLayer(**conv_layer_args)([n, ed, edge_indices])
+        n = CGCNNLayer(**conv_layer_args)([n, edge_distances, edge_indices])
 
     if representation == 'asu':
         out = PoolingWeightedNodes(**node_pooling_args)([n, multiplicities])
@@ -103,18 +111,23 @@ def make_model(inputs,
         out = PoolingNodes(**node_pooling_args)(n)
     
     out = MLP(**output_mlp)(out)
-    
-    if representation == 'unit':
-        model = ks.models.Model(
-                inputs=[atom_attributes, frac_coords, cell_translations, lattice_matrix, edge_indices],
-                outputs=out, name=name)
-    elif representation == 'asu':
-        model = ks.models.Model(
-                inputs=[atom_attributes, frac_coords, multiplicities_inp, cell_translations,
-                    symmops, lattice_matrix, edge_indices], outputs=out, name=name)
+
+    if make_distances:
+        if representation == 'unit':
+            model = ks.models.Model(
+                    inputs=[atom_attributes, frac_coords, cell_translations, lattice_matrix, edge_indices],
+                    outputs=out, name=name)
+        elif representation == 'asu':
+            model = ks.models.Model(
+                    inputs=[atom_attributes, frac_coords, multiplicities_inp, cell_translations,
+                        symmops, lattice_matrix, edge_indices], outputs=out, name=name)
+        else:
+            model = ks.models.Model(
+                    inputs=[atom_attributes, frac_coords, lattice_matrix, edge_indices],
+                    outputs=out, name=name)
     else:
         model = ks.models.Model(
-                inputs=[atom_attributes, frac_coords, lattice_matrix, edge_indices],
+                inputs=[atom_attributes, edge_distances_input, edge_indices],
                 outputs=out, name=name)
     return model
 
