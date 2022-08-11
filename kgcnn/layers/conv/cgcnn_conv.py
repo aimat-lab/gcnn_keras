3eca5090e1bc10dd805a2bb07788df50d9d8e782
PatReis <pat.reiser@web.de>
2022-08-11 17:31:22 +0200

    Co-authored-by: robinruff <robin.ruff.98@gmail.com>

--- kgcnn/layers/conv/cgcnn_conv.py
+++ kgcnn/layers/conv/cgcnn_conv.py
@@ -1,15 +1,16 @@
 import tensorflow as tf
 from kgcnn.layers.conv.message import MessagePassingBase
 from kgcnn.layers.norm import GraphBatchNormalization
+from kgcnn.layers.modules import ActivationEmbedding, LazyMultiply, LazyConcatenate, LazyAdd
 ks = tf.keras
 
 
 @ks.utils.register_keras_serializable(package='kgcnn', name='CGCNNLayer')
 class CGCNNLayer(MessagePassingBase):
-    r"""Message Passing Layer used in the Crystal Graph Convolutional Neural Network (CGCNN).
+    r"""Message Passing Layer used in the Crystal Graph Convolutional Neural Network:
+    `CGCNN <https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.120.145301>`_.
 
-    For more information look at the paper (<https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.120.145301>)
-    or the original code base in pytorch (<https://github.com/txie-93/cgcnn>).
+    Based on the original code in pytorch (<https://github.com/txie-93/cgcnn>).
 
     Args:
         units (int): Units for Dense layer.
@@ -25,17 +26,18 @@ class CGCNNLayer(MessagePassingBase):
                  **kwargs):
         super(CGCNNLayer, self).__init__(**kwargs)
         self.units = units
-        self.activation_s = activation_s
-        self.activation_out = activation_out
         self.batch_normalization = batch_normalization
-        self.activation_f_layer = ks.activations.sigmoid
-        self.activation_s_layer = ks.layers.Activation(activation_s)
-        self.activation_out_layer = ks.layers.Activation(activation_out)
+        self.activation_f_layer = ActivationEmbedding(activation="sigmoid")
+        self.activation_s_layer = ActivationEmbedding(activation_s)
+        self.activation_out_layer = ActivationEmbedding(activation_out)
         self.batch_norm_f = GraphBatchNormalization()
         self.batch_norm_s = GraphBatchNormalization()
         self.batch_norm_out = GraphBatchNormalization()
         self.f = ks.layers.Dense(self.units)
         self.s = ks.layers.Dense(self.units)
+        self.lazy_mult = LazyMultiply()
+        self.lazy_add = LazyAdd()
+        self.lazy_concat = LazyConcatenate()
 
     def message_function(self, inputs, **kwargs):
 
@@ -43,28 +45,30 @@ class CGCNNLayer(MessagePassingBase):
         nodes_out = inputs[1]  # shape: (batch_size, M, F)
         edge_features = inputs[2]  # shape: (batch_size, M, E)
 
-        x = tf.concat([nodes_in, nodes_out, edge_features], axis=2)
-        x_s, x_f = self.s(x), self.f(x)
+        x = self.lazy_concat([nodes_in, nodes_out, edge_features], axis=2, **kwargs)
+        x_s, x_f = self.s(x, **kwargs), self.f(x, **kwargs)
         if self.batch_normalization:
-            x_s, x_f = self.batch_norm_s(x_s), self.batch_norm_f(x_f)
-        x_s, x_f = self.activation_s_layer(x_s), self.activation_f_layer(x_f)
-        x_out = x_s * x_f # shape: (batch_size, M, self.units)
+            x_s, x_f = self.batch_norm_s(x_s, **kwargs), self.batch_norm_f(x_f, **kwargs)
+        x_s, x_f = self.activation_s_layer(x_s, **kwargs), self.activation_f_layer(x_f, **kwargs)
+        x_out = self.lazy_mult(x_s, x_f, **kwargs)  # shape: (batch_size, M, self.units)
         if self.batch_normalization:
-            x_out = self.batch_norm_out(x_out)
-        x_out = self.activation_out_layer(x_out)
+            x_out = self.batch_norm_out(x_out, **kwargs)
+        x_out = self.activation_out_layer(x_out, **kwargs)
         return x_out
 
     def update_nodes(self, inputs, **kwargs):
         nodes = inputs[0]
         nodes_update = inputs[1]
-        return nodes + nodes_update
+        return self.lazy_add(nodes, nodes_update)
 
     def get_config(self):
         """Update layer config."""
         config = super(CGCNNLayer, self).get_config()
         config.update({
             "units": self.units,
-            "activation_s": self.activation_s,
-            "activation_out": self.activation_out,
             "batch_normalization": self.batch_normalization})
+        conf_s = self.activation_s_layer.get_config()
+        conf_out = self.activation_out_layer.get_config()
+        config.update({"activation_s": conf_s["activation"]})
+        config.update({"activation_out": conf_out["activation"]})
         return config
