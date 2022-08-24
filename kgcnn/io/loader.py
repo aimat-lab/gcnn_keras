12403b20fc8c853d069a461307189821c97081bb
PatReis <pat.reiser@web.de>
2022-08-24 12:16:50 +0200

    Add example graph loader

--- kgcnn/io/loader.py
+++ kgcnn/io/loader.py
@@ -1,108 +1,94 @@
 import numpy as np
 import tensorflow as tf
-
+from typing import Union
 from kgcnn.data.utils import ragged_tensor_from_nested_numpy
+from kgcnn.data.base import MemoryGraphDataset
+ks = tf.keras
 
 
-class NumpyTensorList:
-    r"""An utility class to keep lists of arrays. Multiple lists are handled in parallel and therefore should
-    have the same size. The behavior should be similar to :obj:`train_test_split` from scikit-learn.
-    The length of each list should match the length of the dataset and
-    each list-item be e.g. a numpy-tensor for a data-point. The lists could be X, y or different graph attributes.
-    This class is intentionally kept simple but can be built on in the future.
+class GraphBatchLoader(ks.utils.Sequence):
+    r"""Example implementation of a graph batch loader based on :obj:`ks.utils.Sequence`."""
 
-    """
-
-    def __init__(self, *args):
-        """Make a new `NumpyTensorList` object with list of matching data like features and labels of a dataset.
-
-        .. code-block:: python
-
-            import numpy as np
-            data = NumpyTensorList([np.array([0,0,0]), np.array([0])], [np.array([1]), np.array([1,1])])
-            print(data[0].tensor())
+    def __init__(self,
+                 data: Union[list[dict], MemoryGraphDataset],
+                 inputs: Union[dict, list[dict]],
+                 outputs: Union[dict, list[dict]],
+                 batch_size: int = 32,
+                 shuffle: bool = False):
+        """Initialization with data and input information.
 
         Args:
-            args: Multiple lists of np.ndarrays.
+            data (list, MemoryGraphDataset): Any iterable data that implements indexing operator for graph instance.
+                Each graph instance must implement indexing operator for named property.
+            inputs (dict, list):  List of dictionaries that specify graph properties in list via 'name' key.
+                The dict-items match the tensor input for :obj:`tf.keras.layers.Input` layers.
+                Required dict-keys should be 'name' and 'ragged'.
+                Optionally shape information can be included via 'shape'.
+                E.g.: `[{'name': 'edge_indices', 'ragged': True}, {...}, ...]`.
+            outputs (dict, list): List of dictionaries that specify graph properties in list via 'name' key.
+                Required dict-keys should be 'name' and 'ragged'.
+                Optionally shape information can be included via 'shape'.
+                E.g.: `[{'name': 'edge_indices', 'ragged': True}, {...}, ...]`.
+            batch_size (int): Batch size. Default is 32.
+            shuffle (bool): Whether to shuffle data. Default is False.
         """
-        self._tensor_list = [x for x in args]
-        if len(self._tensor_list) <= 0:
-            print("Received empty list input for `NumpyTensorList`. \
-                   Expected one or more list of numpy arrays.")
+        self.data = data
+        self.inputs = inputs
+        self.outputs = outputs
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.indices = np.arange(len(data))
 
-        # check that all have same length
-        self._length_test()
-
-    def _length_test(self):
-        length_test = np.array([len(x) for x in self._tensor_list], dtype="int")
-        if len(length_test) > 0:
-            if not np.all(length_test == length_test[0]):
-                print("Length of list input to `NumpyTensorList` are different.")
-
-    def __getitem__(self, item):
-        r"""Indexing or getitem method to apply to each list and return a new :obj:`NumpyTensorList`.
-
-        Args:
-            item (list, int, slice, np.ndarray): Index or list of indices to collect from each list in self.
-
-        Returns:
-            NumpyTensorList: New :obj:`NumpyTensorList` with only `items` in each list.
-        """
-        if isinstance(item, int):
-            return NumpyTensorList(*[[x[item]] for x in self._tensor_list])
-        elif isinstance(item, slice):
-            return NumpyTensorList(*[x[item] for x in self._tensor_list])
-        elif hasattr(item, "__getitem__"):
-            return NumpyTensorList(*[[x[y] for y in item] for x in self._tensor_list])
-        else:
-            raise ValueError("ERROR:kgcnn: Can not iterate over input of getitem() for `NumpyTensorList`: ", item)
-
-    def tensor(self, ragged=None):
-        """Cast list of numpy arrays to tf.Tensor or tf.RaggedTensor objects.
-
-        Note: A copy of the data may be generated for ragged tensors!
-
-        Args:
-            ragged (list): Information whether a output tensor for corresponding list is ragged. Default is None.
-
-        Returns:
-            list: A list of tf.Tensor or tf.RaggedTensor objects.
-        """
-        # TODO: We can check if tf.RaggedTensor is actually necessary, without relying on ragged-argument.
-        # TODO: Also need to improve casting, e.g. memory usage
-        # TODO: Also ragged_tensor_from_nested_numpy will fail for arbitrary ragged shape
-        if ragged is None:
-            ragged = [False] * len(self._tensor_list)
-        assert len(ragged) == len(self._tensor_list)
-        out_list = []
-        for i, x in enumerate(self._tensor_list):
-            if not ragged[i]:
-                out_list.append(tf.constant(x))
-            else:
-                out_list.append(ragged_tensor_from_nested_numpy(x))
-        return out_list
-
-    def pop(self, index: int):
-        r"""Remove a single item at index from each list within `NumpyTensorList`.
-
-        Args:
-            index (int): Index of item to remove.
-
-        Returns:
-            list: A list of removed items. Number of lists stored by `NumpyTensorList`.
-        """
-        removed_items = []
-        for i, x in enumerate(self._tensor_list):
-            if isinstance(x, list):
-                temp = x.pop(index)
-                removed_items.append(temp)
-            else:
-                removed_items.append(x[index])
-                self._tensor_list[i] = [x[j] for j in range(len(x)) if j != index]
-        return removed_items
+        self._shuffle_indices()
 
     def __len__(self):
-        """Get length of each list."""
-        self._length_test()
-        return len(self._tensor_list[0])
+        """Denotes the number of batches per epoch"""
+        return int(np.ceil(len(self.data) / float(self.batch_size)))
 
+    def __getitem__(self, index):
+        """Generate one batch of data"""
+        # Generate indexes of the batch
+        batch_indices = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
+
+        # Generate data
+        x_model, y_model = self._data_generation(batch_indices)
+
+        # return batch_indexes
+        return x_model, y_model
+
+    def _shuffle_indices(self):
+        if self.shuffle:
+            np.random.shuffle(self.indices)
+
+    def on_epoch_end(self):
+        """Updates after each epoch"""
+        self._shuffle_indices()
+
+    @staticmethod
+    def _to_tensor(item, is_ragged):
+        if is_ragged:
+            return ragged_tensor_from_nested_numpy(item)
+        else:
+            return tf.constant(np.array(item))
+
+    def _data_generation(self, batch_indices):
+        """Generates data containing batch_size samples"""
+        graphs = [self.data[i] for i in batch_indices]
+        # Inputs
+        inputs = self.inputs if not isinstance(self.inputs, dict) else [self.inputs]
+        x_inputs = []
+        for i in inputs:
+            data_list = [g[i["name"]] for g in graphs]
+            is_ragged = i["ragged"] if "ragged" in i else False
+            x_inputs.append(self._to_tensor(data_list, is_ragged))
+        y_outputs = []
+        # Outputs
+        outputs = self.outputs if not isinstance(self.outputs, dict) else [self.outputs]
+        for i in outputs:
+            data_list = [g[i["name"]] for g in graphs]
+            is_ragged = i["ragged"] if "ragged" in i else False
+            y_outputs.append(self._to_tensor(data_list, is_ragged))
+        # Check return type.
+        x_inputs = x_inputs if not isinstance(self.inputs, dict) else x_inputs[0]
+        y_outputs = y_outputs if not isinstance(self.outputs, dict) else y_outputs[0]
+        return x_inputs, y_outputs
