cf4aec11f0ba3173a801ca6776942f16e14f9421
PatReis <pat.reiser@web.de>
2022-09-06 13:53:59 +0200

    Redo training stats

--- kgcnn/crystal/preprocessors.py
+++ kgcnn/crystal/preprocessors.py
@@ -1,6 +1,9 @@
-import numpy as np
-import pymatgen
-import pymatgen.io.cif
+import pyxtal
+# pyxtal requires pymatgen as dependency.
+from pymatgen.core.structure import Structure
+from networkx import MultiDiGraph
+from kgcnn.crystal.base import CrystalPreprocessor
+from . import graph_builder
 from pymatgen.core.structure import Structure
 from kgcnn.crystal.base import CrystalPreprocessor
 from . import graph_builder
@@ -99,7 +102,47 @@ class VoronoiSuperCell(CrystalPreprocessor):
         return g
 
 
-def parse_cif_file_to_structures(cif_file: str):
-    # structure = pymatgen.io.cif.CifParser.from_string(cif_string).get_structures()[0]
-    structures = pymatgen.io.cif.CifParser(cif_file).get_structures()
-    return structures
+class RadiusAsymmetricUnitCell(CrystalPreprocessor):
+
+    node_attributes = ['atomic_number', 'frac_coords', 'coords', 'multiplicity']
+    edge_attributes = ['cell_translation', 'distance', 'symmop', 'offset']
+    graph_attributes = ['lattice_matrix', 'spacegroup']
+
+    def __init__(self, radius=3.0):
+        self.radius = radius
+
+    def __call__(self, structure: Structure):
+        g = graph_builder.structure_to_empty_graph(structure, symmetrize=True)
+        g = graph_builder.add_radius_bonds(g, radius=self.radius, inplace=True)
+        g = graph_builder.add_edge_information(g, inplace=True)
+        g = graph_builder.to_asymmetric_unit_graph(g)
+        return g
+
+class KNNAsymmetricUnitCell(CrystalPreprocessor):
+
+    node_attributes = ['atomic_number', 'frac_coords', 'coords', 'multiplicity']
+    edge_attributes = ['cell_translation', 'distance', 'symmop', 'offset']
+    graph_attributes = ['lattice_matrix', 'spacegroup']
+
+    def __init__(self, k=12):
+        self.k = k
+
+    def __call__(self, structure: Structure) -> MultiDiGraph:
+        g = graph_builder.structure_to_empty_graph(structure, symmetrize=True)
+        g = graph_builder.add_knn_bonds(g, k=self.k, inplace=True)
+        g = graph_builder.add_edge_information(g, inplace=True)
+        g = graph_builder.to_asymmetric_unit_graph(g)
+        return g
+
+class VoronoiAsymmetricUnitCell(CrystalPreprocessor):
+
+    node_attributes = ['atomic_number', 'frac_coords', 'coords', 'multiplicity']
+    edge_attributes = ['cell_translation', 'distance', 'symmop', 'offset']
+    graph_attributes = ['lattice_matrix', 'spacegroup']
+
+    def __call__(self, structure: Structure) -> MultiDiGraph:
+        g = graph_builder.structure_to_empty_graph(structure, symmetrize=True)
+        g = graph_builder.add_voronoi_bonds(g, inplace=True)
+        g = graph_builder.add_edge_information(g, inplace=True)
+        g = graph_builder.to_asymmetric_unit_graph(g)
+        return g
