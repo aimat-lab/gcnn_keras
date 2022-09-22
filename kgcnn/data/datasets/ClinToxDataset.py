4216a42229d5c5dbfca65e8cf1d8d6654bb8ce43
PatReis <pat.reiser@web.de>
2022-09-22 21:05:17 +0200

    Added ClinToxDataset

--- kgcnn/data/datasets/ClinToxDataset.py
+++ kgcnn/data/datasets/ClinToxDataset.py
@@ -5,7 +5,7 @@ from kgcnn.data.datasets.MoleculeNetDataset2018 import MoleculeNetDataset2018
 class ClinToxDataset(MoleculeNetDataset2018):
     """Store and process full ClinTox dataset."""
 
-    def __init__(self, reload=False, verbose=1):
+    def __init__(self, reload=False, verbose=1, label_index: int = 0):
         """Initialize ClinTox dataset.
 
         Args:
@@ -13,13 +13,14 @@ class ClinToxDataset(MoleculeNetDataset2018):
             verbose (int): Print progress or info for processing where 0=silent. Default is 1.
         """
         super(ClinToxDataset, self).__init__("ClinTox", reload=reload, verbose=verbose)
+        self.label_index = label_index
 
     def read_in_memory(self, **kwargs):
         super(ClinToxDataset, self).read_in_memory(**kwargs)
 
         # Strictly one class so we pick drug approved for positive label.
         graph_labels = self.obtain_property("graph_labels")
-        graph_labels = [np.array(x, dtype="float") if x is not None else None for x in graph_labels]
+        graph_labels = [np.array([x[self.label_index]], dtype="float") if x is not None else None for x in graph_labels]
         self.assign_property("graph_labels", graph_labels)
 
 
