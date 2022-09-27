095606ab34694ace85d65c826e6891d39e316fd4
Jonas Teufel <jonseb1998@gmail.com>
2022-09-27 17:08:45 +0200

    basic megan implementation

--- kgcnn/literature/MEGAN.py
+++ kgcnn/literature/MEGAN.py
@@ -3,8 +3,13 @@ from typing import List, Tuple, Dict, Optional, Any
 import tensorflow as tf
 import tensorflow.keras as ks
 
-from kgcnn.layers.conv.attention import AttentionHeadGATV2
 from kgcnn.layers.base import GraphBaseLayer
+from kgcnn.layers.modules import DenseEmbedding
+from kgcnn.layers.modules import ActivationEmbedding, DropoutEmbedding
+from kgcnn.layers.modules import LazyConcatenate, LazyAverage
+from kgcnn.layers.conv.gat_conv import MultiHeadGATV2Layer
+from kgcnn.layers.pooling import PoolingLocalEdges
+from kgcnn.layers.pooling import PoolingWeightedNodes, PoolingNodes
 
 
 class MEGAN(ks.models.Model):
@@ -19,6 +24,7 @@ class MEGAN(ks.models.Model):
                  # node/edge importance related arguments
                  importance_units: List[int] = [],
                  importance_channels: int = 2,
+                 importance_activation: str = 'sigmoid',
                  importance_dropout_rate: float = 0.0,
                  importance_factor: float = 0.0,
                  importance_multiplier: float = 1.0,
@@ -27,6 +33,7 @@ class MEGAN(ks.models.Model):
                  final_units: List[int] = [1],
                  final_dropout_rate: float = 0.0,
                  final_activation: str = 'linear',
+                 final_pooling: str = 'sum',
                  regression_limits: Optional[Tuple[float, float]] = None,
                  regression_reference: Optional[float] = None,
                  **kwargs):
@@ -38,6 +45,7 @@ class MEGAN(ks.models.Model):
         self.use_edge_features = use_edge_features
         self.importance_units = importance_units
         self.importance_channels = importance_channels
+        self.importance_activation = importance_activation
         self.importance_dropout_rate = importance_dropout_rate
         self.importance_factor = importance_factor
         self.importance_multiplier = importance_multiplier
@@ -45,20 +53,106 @@ class MEGAN(ks.models.Model):
         self.final_units = final_units
         self.final_dropout_rate = final_dropout_rate
         self.final_activation = final_activation
+        self.final_pooling = final_pooling
         self.regression_limits = regression_limits
         self.regression_reference = regression_reference
 
+        # ~ MAIN CONVOLUTIONAL / ATTENTION LAYERS
         self.attention_layers: List[GraphBaseLayer] = []
-        for unit in self.units:
-            # lay = MultiHeadGatv2Layer(
-            #     units=units,
-            #     num_heads=self.importance_channels,
-            #     use_edge_features=self.use_edge_features,
-            #     activation=self.activation,
-            #     use_bias=self.use_bias,
-            #     has_self_loops=True,
-            #     concat_heads=True
-            # )
-            # self.attention_layers.append(lay)
-            pass
+        for u in self.units:
+            lay = MultiHeadGATV2Layer(
+                units=u,
+                num_heads=self.importance_channels,
+                use_edge_features=self.use_edge_features,
+                activation=self.activation,
+                use_bias=self.use_bias,
+                has_self_loops=True,
+                concat_heads=True
+            )
+            self.attention_layers.append(lay)
+
+        self.lay_dropout = DropoutEmbedding(rate=self.dropout_rate)
+
+        # ~ EDGE IMPORTANCES
+        self.lay_act_importance = ActivationEmbedding(activation=self.importance_activation)
+        self.lay_concat_alphas = LazyConcatenate(axis=-1)
+
+        self.lay_pool_edges_in = PoolingLocalEdges(pooling_method='mean', pooling_index=0)
+        self.lay_pool_edges_out = PoolingLocalEdges(pooling_method='mean', pooling_index=1)
+        self.lay_average = LazyAverage()
+
+        # ~ NODE IMPORTANCES
+        self.node_importance_units = importance_units + [self.importance_channels]
+        self.node_importance_acts = ['relu' for _ in importance_units] + ['linear']
+        self.node_importance_layers = []
+        for u, act in zip(self.node_importance_units, self.node_importance_acts):
+            lay = DenseEmbedding(
+                units=u,
+                activation=act,
+                use_bias=use_bias
+            )
+            self.node_importance_layers.append(lay)
+
+        # ~ OUTPUT / MLP TAIL END
+        self.lay_pool_out = PoolingNodes(pooling_method=self.final_pooling)
+        self.lay_concat_out = LazyConcatenate(axis=-1)
+        self.lay_final_dropout = DropoutEmbedding(rate=self.final_dropout_rate)
+
+        self.final_acts = ['relu' for _ in self.final_units]
+        self.final_acts[-1] = self.final_activation
+        self.final_layers = []
+        for u, act in zip(self.final_units, self.final_acts):
+            lay = DenseEmbedding(
+                units=u,
+                activation=act,
+                use_bias=use_bias
+            )
+            self.final_layers.append(lay)
+
+        # ~ EXPLANATION ONLY TRAIN STEP
+
+    def call(self,
+             inputs):
+
+        node_input, edge_input, edge_index_input = inputs
+
+        alphas = []
+        x = node_input
+        for lay in self.attention_layers:
+            x, alpha = lay([x, edge_input, edge_index_input])
+            x = self.lay_dropout(x)
+
+            alphas.append(alpha)
+
+        alphas = self.lay_concat_alphas(alphas)
+        edge_importances = tf.reduce_sum(alphas, axis=-1, keepdims=False)
+        edge_importances = self.lay_act_importance(edge_importances)
+
+        pooled_edges_in = self.lay_pool_edges_in([node_input, edge_importances, edge_index_input])
+        pooled_edges_out = self.lay_pool_edges_out([node_input, edge_importances, edge_index_input])
+        pooled_edges = self.lay_average([pooled_edges_out, pooled_edges_in])
+
+        node_importances_tilde = x
+        for lay in self.node_importance_layers:
+            node_importances_tilde = lay(node_importances_tilde)
+
+        node_importances_tilde = self.lay_act_importance(node_importances_tilde)
+
+        node_importances = node_importances_tilde * pooled_edges
+
+        outs = []
+        for k in range(self.importance_channels):
+            node_importance_slice = tf.expand_dims(node_importances[:, :, k], axis=-1)
+            out = self.lay_pool_out(x * node_importance_slice)
+
+            outs.append(out)
+
+        out = self.lay_concat_out(outs)
+
+        for lay in self.final_layers:
+            out = lay(out)
+            self.lay_final_dropout(out)
+
+        return out, node_importances, edge_importances
+
 
--- test/test_literature_megan.py
+++ test/test_literature_megan.py
@@ -1,4 +1,6 @@
 import unittest
+import random
+import itertools
 
 import tensorflow as tf
 import tensorflow.keras as ks
@@ -8,7 +10,50 @@ from kgcnn.literature.MEGAN import MEGAN
 
 class TestMegan(unittest.TestCase):
 
+    def random_input(self, num_batches, num_features):
+        n = []
+        ei = []
+        e = []
+        for b in range(num_batches):
+            N = random.randint(5, 30)
+            node_indices = list(range(N))
+            M = random.randint(1, N - 1)
+
+            n.append([[random.random() for _ in range(num_features)] for _ in range(N)])
+            e.append([[random.random() for _ in range(num_features)] for _ in range(M)])
+            ei.append(list(zip(random.sample(node_indices, M), random.sample(node_indices, M))))
+
+        return (
+            tf.ragged.constant(n, ragged_rank=1),
+            tf.ragged.constant(e, ragged_rank=1),
+            tf.ragged.constant(ei, ragged_rank=1),
+        )
+
+    # -- UNITTESTS --
+
     def test_construction_basically_works(self):
         model = MEGAN(units=[1])
         self.assertIsInstance(model, MEGAN)
         self.assertIsInstance(model, ks.models.Model)
+
+    def test_shapes_basically_work(self):
+        num_batches = 5
+        num_features = 3
+        n, e, ei = self.random_input(num_batches=num_batches, num_features=num_features)
+
+        # We just test a bunch of different configurations for the output dimension and the number of
+        # explanation channels.
+        output_dimensions = [1, 3]
+        channel_dimensions = [2, 6]
+
+        for num_out, num_channels in itertools.product(output_dimensions, channel_dimensions):
+            model = MEGAN(
+                units=[3],
+                importance_channels=num_channels,
+                final_units=[num_out],
+            )
+
+            out, node_importances, edge_importances = model([n, e, ei])
+            self.assertEqual((num_batches, num_out), out.shape)
+            self.assertEqual((num_batches, None, num_channels), node_importances.shape)
+            self.assertEqual((num_batches, None, num_channels), node_importances.shape)
