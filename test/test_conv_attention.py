63a2c45cb77cd7314db0a2c7c26f71d031b2a4bd
the16thpythonist <jonseb1998@gmail.com>
2022-12-04 12:05:04 +0100

    fixed test_conv_attention.py

--- test/test_conv_attention.py
+++ test/test_conv_attention.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 
 from kgcnn.layers.casting import ChangeTensorType
 from kgcnn.layers.base import GraphBaseLayer
-from kgcnn.layers.conv.attention import PoolingLocalEdgesAttention
+from kgcnn.layers.conv.gat_conv import PoolingLocalEdgesAttention
 from kgcnn.layers.conv.gat_conv import AttentionHeadGAT
 from kgcnn.layers.conv.gat_conv import MultiHeadGATV2Layer
 
