5279227c34f911338a1eb2b5c2397897d93357f0
PatReis <pat.reiser@web.de>
2023-01-11 09:57:50 +0100

    Added representation model to HDNNP2nd.py

--- kgcnn/literature/HDNNP2nd.py
+++ kgcnn/literature/HDNNP2nd.py
@@ -13,7 +13,7 @@ ks = tf.keras
 
 # Keep track of model version from commit date in literature.
 # To be updated if model is changed in a significant way.
-__model_version__ = "2023.01.10"
+__model_version__ = "2023.01.11"
 
 # Implementation of HDNNP in `tf.keras` from paper:
 # Atom-centered symmetry functions for constructing high-dimensional neural network potentials
@@ -151,19 +151,19 @@ model_default_behler = {
 
 
 @update_model_kwargs(model_default_behler)
-def model_model_behler(inputs: list = None,
-                       node_pooling_args: dict = None,
-                       name: str = None,
-                       verbose: int = None,
-                       normalize_kwargs: dict = None,
-                       g2_kwargs: dict = None,
-                       g4_kwargs: dict = None,
-                       mlp_kwargs: dict = None,
-                       output_embedding: str = None,
-                       use_output_mlp: bool = None,
-                       output_to_tensor: bool = None,
-                       output_mlp: dict = None
-                       ):
+def make_model_behler(inputs: list = None,
+                      node_pooling_args: dict = None,
+                      name: str = None,
+                      verbose: int = None,
+                      normalize_kwargs: dict = None,
+                      g2_kwargs: dict = None,
+                      g4_kwargs: dict = None,
+                      mlp_kwargs: dict = None,
+                      output_embedding: str = None,
+                      use_output_mlp: bool = None,
+                      output_to_tensor: bool = None,
+                      output_mlp: dict = None
+                      ):
     r"""Make 2nd generation `HDNNP <https://arxiv.org/abs/1706.08566>`_ graph network via functional API.
 
     Default parameters can be found in :obj:`kgcnn.literature.HDNNP2nd.model_default_behler`.
@@ -231,6 +231,82 @@ def model_model_behler(inputs: list = None,
 
     model = ks.models.Model(
         inputs=[node_input, xyz_input, edge_index_input, angle_index_input], outputs=out, name=name)
+
+    model.__kgcnn_model_version__ = __model_version__
+    return model
+
+
+default_representation_behler = {
+    "name": "HDNNP2nd",
+    "inputs": [{"shape": (None,), "name": "node_number", "dtype": "int64", "ragged": True},
+               {"shape": (None, 3), "name": "node_coordinates", "dtype": "float32", "ragged": True},
+               {"shape": (None, 2), "name": "edge_indices", "dtype": "int64", "ragged": True},
+               {"shape": (None, 3), "name": "angle_indices_nodes", "dtype": "int64", "ragged": True}],
+    "g2_kwargs": {"eta": [0.0, 0.3], "rs": [0.0, 3.0], "rc": 10.0, "elements": [1, 6, 16]},
+    "g4_kwargs": {"eta": [0.0, 0.3], "lamda": [-1.0, 1.0], "rc": 6.0,
+                  "zeta": [1.0, 8.0], "elements": [1, 6, 16], "multiplicity": 2.0},
+    "normalize_kwargs": None,
+    "verbose": 10,
+    "output_to_tensor": False,
+}
+
+
+@update_model_kwargs(default_representation_behler)
+def make_representation_behler(inputs: list = None,
+                               name: str = None,
+                               verbose: int = None,
+                               normalize_kwargs: dict = None,
+                               g2_kwargs: dict = None,
+                               g4_kwargs: dict = None,
+                               output_to_tensor: bool = None,
+                               ):
+    r"""Make ACSF representation only for 2nd generation `HDNNP <https://arxiv.org/abs/1706.08566>`_ .
+
+    Default parameters can be found in :obj:`kgcnn.literature.HDNNP2nd.default_representation_behler`.
+
+    Inputs:
+        list: `[node_number, node_coordinates, edge_indices, angle_indices_nodes]`
+
+            - node_number (tf.RaggedTensor): Atomic number of shape `(batch, None)` .
+            - node_coordinates (tf.RaggedTensor): Node (atomic) coordinates of shape `(batch, None, 3)`.
+            - edge_indices (tf.RaggedTensor): Index list for edges of shape `(batch, None, 2)`.
+            - angle_indices_nodes (tf.RaggedTensor): Index list for angles of shape `(batch, None, 3)`.
+
+    Outputs:
+        tf.Tensor: Node representation of shape `(batch, None, L)` .
+
+    Args:
+        inputs (list): List of dictionaries unpacked in :obj:`tf.keras.layers.Input`. Order must match model definition.
+        verbose (int): Level of verbosity.
+        name (str): Name of the model.
+        g2_kwargs (dict): Dictionary of layer arguments unpacked in :obj:`ACSFG2` layer.
+        g4_kwargs (dict): Dictionary of layer arguments unpacked in :obj:`ACSFG4` layer.
+        normalize_kwargs (dict): Dictionary of layer arguments unpacked in :obj:`GraphBatchNormalization` layer.
+        output_to_tensor (bool): Whether to cast model output to :obj:`tf.Tensor`.
+
+    Returns:
+        :obj:`tf.keras.models.Model`
+    """
+    # Make input
+    node_input = ks.layers.Input(**inputs[0])
+    xyz_input = ks.layers.Input(**inputs[1])
+    edge_index_input = ks.layers.Input(**inputs[2])
+    angle_index_input = ks.layers.Input(**inputs[3])
+
+    # ACSF representation.
+    rep_g2 = ACSFG2(**ACSFG2.make_param_table(**g2_kwargs))([node_input, xyz_input, edge_index_input])
+    rep_g4 = ACSFG4(**ACSFG4.make_param_table(**g4_kwargs))([node_input, xyz_input, angle_index_input])
+    n = LazyConcatenate()([rep_g2, rep_g4])
+
+    # Normalization
+    if normalize_kwargs:
+        n = GraphBatchNormalization(**normalize_kwargs)(n)
+
+    if output_to_tensor:  # For tf version < 2.8 cast to tensor below.
+        n = ChangeTensorType(input_tensor_type="ragged", output_tensor_type="tensor")(n)
+
+    model = ks.models.Model(
+        inputs=[node_input, xyz_input, edge_index_input, angle_index_input], outputs=n, name=name)
 
     model.__kgcnn_model_version__ = __model_version__
     return model
