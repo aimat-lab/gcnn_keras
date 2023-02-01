9da651ee2bfa51313163b4ad6b02d7a66f2899bd
PatReis <pat.reiser@web.de>
2023-02-01 17:16:58 +0100

    Notebook for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,8 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinx.ext.napoleon',
-    'IPython.sphinxext.ipython_console_highlighting'
+    'nbsphinx',  # This lets us use notebooks
+    # 'IPython.sphinxext.ipython_console_highlighting'
 ]
 
 napoleon_google_docstring = True
@@ -51,6 +52,14 @@ napoleon_include_special_with_doc = True
 napoleon_include_private_with_doc = True
 napoleon_include_init_with_doc = True
 # autoclass_content = 'both'
+
+# I execute the notebooks manually in advance. If notebooks test the code,
+# they should be run at build time.
+nbsphinx_execute = 'never'
+nbsphinx_allow_errors = True
+
+# Add type of source files
+source_suffix = ['.rst', '.md', '.ipynb']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
