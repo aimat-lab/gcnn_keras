854215b3278c74c0be2d4c974776927c9900cf17
PatReis <pat.reiser@web.de>
2023-02-15 14:44:44 +0100

    Added transform_dataset methods to scaler.py .

---
+++ kgcnn/scaler/scaler.py
@@ -0,0 +1,6 @@
+from kgcnn.data.transform.scaler.scaler import *
+import logging
+
+# For old reference.
+logging.error(
+    "Module '%s' is deprecated and will be removed in future versions. Please move to 'kgcnn.data.transform.scaler'." % __name__)
