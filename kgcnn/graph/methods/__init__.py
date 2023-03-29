1c056f9a3b2990a1adb176c2dcc58c86d2ff64cf
PatReis <pat.reiser@web.de>
2023-03-29 13:59:01 +0200

    Refactor mol and moldyn modules

--- kgcnn/graph/methods/__init__.py
+++ kgcnn/graph/methods/__init__.py
@@ -6,15 +6,18 @@ from ._adj import (
 )
 from _geom import (
     range_neighbour_lattice, get_principal_moments_of_inertia,
-    shift_coordinates_to_unit_cell, distance_for_range_indices, distance_for_range_indices_periodic
+    shift_coordinates_to_unit_cell, distance_for_range_indices, distance_for_range_indices_periodic,
+    coulomb_matrix_to_inverse_distance_proton, coordinates_from_distance_matrix
 )
 
 __all__ = [
+    # adj
     "get_angle_indices", "coordinates_to_distancematrix", "invert_distance",
     "define_adjacency_from_distance", "sort_edge_indices", "get_angle", "add_edges_reverse_indices",
     "rescale_edge_weights_degree_sym", "add_self_loops_to_edge_indices", "compute_reverse_edges_index_map",
     "distance_to_gauss_basis",
-
+    # geom
     "range_neighbour_lattice", "get_principal_moments_of_inertia",
-    "shift_coordinates_to_unit_cell", "distance_for_range_indices", "distance_for_range_indices_periodic"
+    "shift_coordinates_to_unit_cell", "distance_for_range_indices", "distance_for_range_indices_periodic",
+    "coulomb_matrix_to_inverse_distance_proton", "coordinates_from_distance_matrix"
 ]
