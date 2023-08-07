18e6395780135ef34e47db10d0702d39057d5cc9
PatReis <pat.reiser@web.de>
2023-08-07 17:14:35 +0200

    Keep pooling module with warning.

--- kgcnn/layers/pooling.py
+++ kgcnn/layers/pooling.py
@@ -1,1 +1,9 @@
 from kgcnn.layers.aggr import *
+import logging
+
+# Module logger
+logging.basicConfig()
+module_logger = logging.getLogger(__name__)
+module_logger.setLevel(logging.INFO)
+
+module_logger.warning("Module `pooling` has been renamed `aggr` for better compatibility with other libraries. Please change module name.")
