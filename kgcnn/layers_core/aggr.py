80a83b5cf70ac9c6c4d9776c862505caa3071440
PatReis <pat.reiser@web.de>
2023-09-08 16:57:47 +0200

    continue keras core integration

--- kgcnn/layers_core/aggr.py
+++ kgcnn/layers_core/aggr.py
@@ -2,7 +2,8 @@ import keras_core as ks
 import keras_core.saving
 from keras_core.layers import Layer
 from keras_core import ops
-from kgcnn.ops_core.scatter import scatter_reduce_min, scatter_reduce_mean, scatter_reduce_max, scatter_reduce_sum
+from kgcnn.ops_core.scatter import (
+    scatter_reduce_min, scatter_reduce_mean, scatter_reduce_max, scatter_reduce_sum, scatter_reduce_softmax)
 
 
 @ks.saving.register_keras_serializable(package='kgcnn', name='Aggregate')
@@ -94,7 +95,73 @@ class AggregateWeightedLocalEdges(AggregateLocalEdges):
         out = self.to_aggregate([edges, edge_index[self.pooling_index], n])
 
         if self.normalize_by_weights:
-            norm = self.to_aggregate_weights([
-                weights, edge_index[self.pooling_index], n])
+            norm = self.to_aggregate_weights([weights, edge_index[self.pooling_index], n])
             out = out/norm
         return out
+
+
+class AggregateLocalEdgesAttention(Layer):
+    r"""Aggregate local edges via Attention mechanism.
+    Uses attention for pooling. i.e. :math:`n_i =  \sum_j \alpha_{ij} e_{ij}`
+    The attention is computed via: :math:`\alpha_ij = \text{softmax}_j (a_{ij})` from the
+    attention coefficients :math:`a_{ij}` .
+    The attention coefficients must be computed beforehand by edge features or by :math:`\sigma( W n_i || W n_j)` and
+    are passed to this layer as input. Thereby this layer has no weights and only does pooling.
+    In summary, :math:`n_i = \sum_j \text{softmax}_j (a_{ij}) e_{ij}` is computed by the layer.
+    """
+
+    def __init__(self,
+                 pooling_method="scatter_reduce_softmax",
+                 pooling_index: int = 1,
+                 is_sorted: bool = False,
+                 has_unconnected: bool = True,
+                 **kwargs):
+        """Initialize layer.
+
+        Args:
+            pooling_method (str): Pooling method for this layer.
+            pooling_index (int): Index to pick ID's for pooling edge-like embeddings. Default is 0.
+            is_sorted (bool): If the edge indices are sorted for first ingoing index. Default is False.
+            has_unconnected (bool): If unconnected nodes are allowed. Default is True.
+        """
+        super(AggregateLocalEdgesAttention, self).__init__(**kwargs)
+        self.pooling_method = pooling_method
+        self.pooling_index = pooling_index
+        self.is_sorted = is_sorted
+        self.has_unconnected = has_unconnected
+        pooling_by_name = {
+            "scatter_softmax": scatter_reduce_softmax,
+        }
+        self._pool_method = pooling_by_name[pooling_method]
+        self._use_scatter = "scatter" in pooling_method
+
+    def build(self, input_shape):
+        """Build layer."""
+        super(AggregateLocalEdgesAttention, self).build(input_shape)
+
+    def call(self, inputs, **kwargs):
+        """Forward pass.
+
+        Args:
+            inputs: [node, edges, attention, edge_indices]
+
+                - nodes (Tensor): Node embeddings of shape (N, F)
+                - edges (Tensor): Edge or message embeddings of shape (M, F)
+                - attention (Tensor): Attention coefficients of shape (M, 1)
+                - edge_indices (Tensor): Edge indices referring to nodes of shape (M, 2)
+
+        Returns:
+            Tensor: Embedding tensor of aggregated edge attentions for each node of shape (N, F)
+        """
+        reference, x, attention, disjoint_indices = inputs
+        receive_indices = disjoint_indices[self.pooling_index]
+        shape = ops.shape(reference)[:1] + ops.shape(x)[1:]
+        if self._use_scatter:
+            return self._pool_method(ops.expand_dims(receive_indices, axis=-1), x, attention, shape=shape)
+        else:
+            raise NotImplementedError()
+
+    def get_config(self):
+        """Update layer config."""
+        config = super(AggregateLocalEdgesAttention, self).get_config()
+        return config
--- kgcnn/ops_core/scatter.py
+++ kgcnn/ops_core/scatter.py
@@ -58,3 +58,17 @@ def scatter_reduce_sum(indices, values, shape):
     if any_symbolic_tensors((indices, values, shape)):
         return ScatterSum().symbolic_call(indices, values, shape)
     return kgcnn_backend.scatter_reduce_sum(indices, values, shape)
+
+
+class ScatterSoftmax(Operation):
+    def call(self, indices, values, attention, shape):
+        return kgcnn_backend.scatter_reduce_softmax(indices, values, attention, shape)
+
+    def compute_output_spec(self, indices, values, attention, shape):
+        return KerasTensor(shape, dtype=values.dtype)
+
+
+def scatter_reduce_softmax(indices, values, attention, shape):
+    if any_symbolic_tensors((indices, values, attention, shape)):
+        return ScatterSoftmax().symbolic_call(indices, values, attention, shape)
+    return kgcnn_backend.scatter_reduce_softmax(indices, values, attention, shape)
