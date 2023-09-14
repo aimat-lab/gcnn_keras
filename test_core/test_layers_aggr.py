297a9338c94641764868947df48eb16d0bebbd7f
PatReis <pat.reiser@web.de>
2023-09-14 14:37:09 +0200

    continue keras core integration

--- test_core/test_layers_aggr.py
+++ test_core/test_layers_aggr.py
@@ -5,7 +5,6 @@ from kgcnn.layers_core.aggr import AggregateLocalEdges, AggregateLocalEdgesAtten
 
 
 class TestAggregateLocalEdges(testing.TestCase):
-
     node_attr = np.array([[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]])
     edge_attr = np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 1.0], [0.0, 1.0, 0.0], [1.0, 1.0, 1.0],
                           [1.0, 0.0, 0.0], [1.0, 0.0, 1.0], [1.0, 1.0, 0.0], [1.0, 1.0, 1.0]])
@@ -14,14 +13,12 @@ class TestAggregateLocalEdges(testing.TestCase):
     batch = np.array([0, 0, 1, 1])
 
     def test_correctness(self):
-
         layer = AggregateLocalEdges()
         nodes_aggr = layer([self.node_attr, self.edge_attr, ops.cast(self.edge_index, dtype="int64")])
         expected_output = np.array([[0., 1., 0.], [1., 1., 2.], [2., 1., 0.], [2., 1., 2.]])
         self.assertAllClose(nodes_aggr, expected_output)
 
     def test_basics(self):
-
         self.run_layer_test(
             AggregateLocalEdges,
             init_kwargs={
@@ -39,7 +36,6 @@ class TestAggregateLocalEdges(testing.TestCase):
 
 
 class TestAggregateLocalEdgesAttention(testing.TestCase):
-
     node_attr = np.array([[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]])
     edge_attr = np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 1.0], [0.0, 1.0, 0.0], [1.0, 1.0, 1.0],
                           [1.0, 0.0, 0.0], [1.0, 0.0, 1.0], [1.0, 1.0, 0.0], [1.0, 1.0, 1.0]])
@@ -48,39 +44,19 @@ class TestAggregateLocalEdgesAttention(testing.TestCase):
     batch = np.array([0, 0, 1, 1])
 
     def test_correctness(self):
-
         layer = AggregateLocalEdgesAttention()
         nodes_aggr = layer([self.node_attr, self.edge_attr, self.edge_att, ops.cast(self.edge_index, dtype="int64")])
 
-        from kgcnn.layers.aggr import AggregateLocalEdgesAttention as a
-        import tensorflow as tf
-        print(a()([tf.rage]))
-        expected_output =
+        expected_output = [[0.0000000e+00, 0.0000000e+00, 8.1757444e-01],
+                           [7.3105860e-01, 1.0000000e+00, 7.3105860e-01],
+                           [1.0000000e+00, 0.0000000e+00, 5.0000000e-01],
+                           [1.0000000e+00, 1.0000000e+00, 1.2339458e-04]]
 
         self.assertAllClose(nodes_aggr, expected_output)
 
-    def test_basics(self):
-
-        self.run_layer_test(
-            AggregateLocalEdgesAttention,
-            init_kwargs={
-            },
-            input_dtype="int64",
-            input_shape=[(4, 2), (8, 3), (2, 8)],
-            expected_output_shape=(4, 3),
-            expected_num_trainable_weights=0,
-            expected_num_non_trainable_weights=0,
-            expected_num_seed_generators=0,
-            expected_num_losses=0,
-            supports_masking=False,
-            run_training_check=False,
-        )
-
 
 if __name__ == "__main__":
-
     TestAggregateLocalEdges().test_correctness()
     TestAggregateLocalEdges().test_basics()
     TestAggregateLocalEdgesAttention().test_correctness()
-    TestAggregateLocalEdgesAttention().test_basics()
     print("Tests passed.")
