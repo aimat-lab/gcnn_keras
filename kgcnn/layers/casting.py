c21861a8ffb1c4d1680f658719590572c2de04be
PatReis <pat.reiser@web.de>
2023-10-23 12:54:54 +0200

    refactoring for keras 3.0

--- kgcnn/layers/casting.py
+++ kgcnn/layers/casting.py
@@ -14,25 +14,57 @@ def cat_one(t):
     return ops.concatenate([ops.convert_to_tensor([1], dtype=t.dtype), t], axis=0)
 
 
-class CastBatchedIndicesToDisjoint(Layer):
-    """Cast batched node and edge indices to a (single) disjoint graph representation of
-    `Pytorch Geometric (PyG) <>`__ .
-    For PyG a batch of graphs is represented by single graph which contains disjoint sub-graphs,
-    and the batch information is passed as batch ID tensor: `graph_id_node` and `graph_id_edge` .
-
-    Keras layers can pass unstacked tensors without batch dimension, however, for model input and output
-    batched tensors is currently built in the framework.
-    """
+class _CastDisjointBase(Layer):
 
     def __init__(self, reverse_indices: bool = False, dtype_batch: str = "int64", dtype_index=None,
                  padded_disjoint: bool = False, ragged: bool = False, **kwargs):
-        super(CastBatchedIndicesToDisjoint, self).__init__(**kwargs)
+        r"""Initialize layer.
+
+        Args:
+            reverse_indices (bool): Whether to reverse index order. Default is False.
+            dtype_batch (str): Dtype for batch ID tensor. Default is 'int64'.
+            dtype_index (str): Dtype for index tensor. Default is None.
+            padded_disjoint (bool): Whether to keep padding in disjoint representation. Default is False.
+            ragged (bool): Whether input is ragged. Default is False.
+        """
+        super(_CastDisjointBase, self).__init__(**kwargs)
         self.reverse_indices = reverse_indices
         self.dtype_index = dtype_index
         self.dtype_batch = dtype_batch
         self._has_ragged_input = ragged
         self.padded_disjoint = padded_disjoint
         self.supports_jit = padded_disjoint
+
+    def get_config(self):
+        """Get config dictionary for this layer."""
+        config = super(_CastDisjointBase, self).get_config()
+        config.update({"reverse_indices": self.reverse_indices, "dtype_batch": self.dtype_batch,
+                       "dtype_index": self.dtype_index, "padded_disjoint": self.padded_disjoint,
+                       "ragged": self._has_ragged_input})
+        return config
+
+
+class CastBatchedIndicesToDisjoint(_CastDisjointBase):
+    r"""Cast batched node and edge indices to a (single) disjoint graph representation of
+    `Pytorch Geometric (PyG) <https://github.com/pyg-team/pytorch_geometric>`__ .
+    For PyG a batch of graphs is represented by single graph which contains disjoint sub-graphs,
+    and the batch information is passed as batch ID tensor: `graph_id_node` and `graph_id_edge` .
+
+    Keras layers can pass unstacked tensors without batch dimension, however, for model input and output
+    batched tensors is most natural to the framework. Therefore, this layer can cast to disjoint from ragged or padded
+    input and also keep padding in disjoint representation for jax.
+
+    For padded disjoint all padded nodes are assigned to a padded first empty graph, with single node and at least
+    a single self-loop. This graph therefore does not interact with the actual graphs in the message passing.
+
+    .. warning::
+
+        However, for special operations such as :obj:`GraphBatchNormalization` the information of :obj:`padded_disjoint`
+        must be separately provided, otherwise this will lead to unwanted behaviour.
+    """
+
+    def __init__(self,  **kwargs):
+        super(CastBatchedIndicesToDisjoint, self).__init__(**kwargs)
 
     def build(self, input_shape):
         """Build layer."""
@@ -72,17 +104,17 @@ class CastBatchedIndicesToDisjoint(Layer):
         return out_n, out_i, out_gn, out_ge, out_id_n, out_id_e, out_size_n, out_size_e
 
     def call(self, inputs: list, **kwargs):
-        """Changes node and edge indices into a Pytorch Geometric (PyG) compatible tensor format.
+        r"""Changes node and edge indices into a Pytorch Geometric (PyG) compatible tensor format.
 
         Args:
-            inputs (list): List of `[nodes, edges, edge_indices, nodes_in_batch, edges_in_batch]` ,
+            inputs (list): List of `[nodes, edge_indices, nodes_in_batch, edges_in_batch]` ,
 
                 - nodes (Tensor): Node features are represented by a keras tensor of shape `(batch, N, F, ...)` ,
                   where N denotes the number of nodes.
                 - edge_indices (Tensor): Edge index list have shape `(batch, M, 2)` with the indices of M directed
                   edges at last axis for each edge.
-                - total_nodes (Tensor):
-                - total_edges (Tensor):
+                - total_nodes (Tensor): Tensor of number of nodes for each graph of shape `(batch, )` .
+                - total_edges (Tensor): Tensor of number of edges for each graph of shape `(batch, )` .
 
 
         Returns:
@@ -90,8 +122,8 @@ class CastBatchedIndicesToDisjoint(Layer):
 
                 - node_attr (Tensor): Represents node attributes or coordinates of shape `([N], F, ...)` ,
                 - edge_index (Tensor): Represents the index table of shape `(2, [M])` for directed edges.
-                - graph_id_node (Tensor):
-                - graph_id_edge (Tensor):
+                - graph_id_node (Tensor): ID tensor of graph assignment in disjoint graph of shape `([N], )` .
+                - graph_id_edge (Tensor): ID tensor of graph assignment in disjoint graph of shape `([M], )` .
                 - nodes_id (Tensor): The ID-tensor to assign each node to its respective graph of shape `([N], )` .
                 - edges_id (Tensor): The ID-tensor to assign each edge to its respective batch of shape `([M], )` .
                 - nodes_count (Tensor): Tensor of number of nodes for each graph of shape `(batch, )` .
@@ -193,27 +225,28 @@ class CastBatchedIndicesToDisjoint(Layer):
 
         return [nodes_flatten, disjoint_indices, graph_id_node, graph_id_edge, node_id, edge_id, node_len, edge_len]
 
-    def get_config(self):
-        """Get config dictionary for this layer."""
-        config = super(CastBatchedIndicesToDisjoint, self).get_config()
-        config.update({"reverse_indices": self.reverse_indices, "dtype_batch": self.dtype_batch,
-                       "dtype_index": self.dtype_index})
-        return config
 
+class CastBatchedAttributesToDisjoint(_CastDisjointBase):
+    r"""Cast batched node and edge attributes to a (single) disjoint graph representation of
+    `Pytorch Geometric (PyG) <https://github.com/pyg-team/pytorch_geometric>`__ .
 
-class CastBatchedAttributesToDisjoint(Layer):
+    Only applies a casting of attribute tensors similar to :obj:`CastBatchedIndicesToDisjoint` but without any
+    index adjustment. Produces the batch-ID tensor assignment.
 
-    def __init__(self, reverse_indices: bool = False, dtype_batch: str = "int64", dtype_index=None,
-                 padded_disjoint: bool = False, ragged: bool = False, **kwargs):
+    For padded disjoint all padded nodes are assigned to a padded first empty graph, with single node and at least
+    a single self-loop. This graph therefore does not interact with the actual graphs in the message passing.
+
+    .. warning::
+
+        However, for special operations such as :obj:`GraphBatchNormalization` the information of :obj:`padded_disjoint`
+        must be separately provided, otherwise this will lead to unwanted behaviour.
+    """
+
+    def __init__(self, **kwargs):
         super(CastBatchedAttributesToDisjoint, self).__init__(**kwargs)
-        self.reverse_indices = reverse_indices
-        self.dtype_batch = dtype_batch
-        self._has_ragged_input = ragged
-        self.padded_disjoint = padded_disjoint
-        self.supports_jit = padded_disjoint
-        self.dtype_index = dtype_index
 
     def build(self, input_shape):
+        """Build layer."""
         self.built = True
 
     def compute_output_spec(self, inputs_spec):
@@ -225,6 +258,7 @@ class CastBatchedAttributesToDisjoint(Layer):
         return output_spec
 
     def compute_output_shape(self, input_shape):
+        """Compute output shape as possible."""
         in_n, in_size_n = input_shape
         if not self.padded_disjoint:
             out_n = tuple([None] + list(in_n[2:]))
@@ -246,14 +280,14 @@ class CastBatchedAttributesToDisjoint(Layer):
 
                 - attr (Tensor): Features are represented by a keras tensor of shape `(batch, N, F, ...)` ,
                   where N denotes the number of nodes or edges.
-                - total_attr (Tensor):
+                - total_attr (Tensor): Tensor of lengths for each graph of shape `(batch, )` .
 
         Returns:
             list: `[attr, graph_id, item_id, item_counts]` .
 
                 - attr (Tensor): Represents attributes or coordinates of shape `([N], F, ...)`
-                - graph_id (Tensor):
-                - item_id (Tensor):
+                - graph_id (Tensor): ID tensor of graph assignment in disjoint graph of shape `([N], )` .
+                - item_id (Tensor): The ID-tensor to assign each node to its respective graph of shape `([N], )` .
                 - item_counts (Tensor): Tensor of lengths for each graph of shape `(batch, )` .
         """
         # Case: Ragged Tensor input.
@@ -296,23 +330,17 @@ class CastBatchedAttributesToDisjoint(Layer):
 
         return [nodes_flatten, graph_id_node, node_id, node_len]
 
-    def get_config(self):
-        """Get config dictionary for this layer."""
-        config = super(CastBatchedAttributesToDisjoint, self).get_config()
-        config.update({"dtype_batch": self.dtype_batch})
-        return config
 
+class CastDisjointToGraphState(_CastDisjointBase):
+    r"""Cast graph property tensor from disjoint graph representation of
+    `Pytorch Geometric (PyG) <https://github.com/pyg-team/pytorch_geometric>`__ .
 
-class CastDisjointToGraphState(Layer):
+    The graph state is usually kept as batched tensor, except for padded disjoint representation, an empty zero valued
+    graph is removed that represents all padded nodes.
+    """
 
-    def __init__(self, reverse_indices: bool = False, dtype_batch: str = "int64", dtype_index=None,
-                 padded_disjoint: bool = False, **kwargs):
+    def __init__(self, **kwargs):
         super(CastDisjointToGraphState, self).__init__(**kwargs)
-        self.reverse_indices = reverse_indices
-        self.dtype_batch = dtype_batch
-        self.dtype_index = dtype_index
-        self.supports_jit = True
-        self.padded_disjoint = padded_disjoint
 
     def build(self, input_shape):
         self.built = True
@@ -327,32 +355,27 @@ class CastDisjointToGraphState(Layer):
         """Changes graph tensor from disjoint representation.
 
         Args:
-            inputs (Tensor): Graph labels from a disjoint representation of shape `(batch, ...)` .
+            inputs (Tensor): Graph labels from a disjoint representation of shape `(batch, ...)` or
+                `(batch+1, ...)` for padded disjoint.
 
         Returns:
             Tensor: Graph labels of shape `(batch, ...)` .
         """
+        # Simply remove first graph.
         if self.padded_disjoint:
             return inputs[1:]
         return inputs
 
-    def get_config(self):
-        """Get config dictionary for this layer."""
-        config = super(CastDisjointToGraphState, self).get_config()
-        config.update({"dtype_batch": self.dtype_batch})
-        return config
 
+class CastDisjointToBatchedAttributes(_CastDisjointBase):
+    r"""Cast batched node and edge attributes from a (single) disjoint graph representation of
+    `Pytorch Geometric (PyG) <https://github.com/pyg-team/pytorch_geometric>`__ .
 
-class CastDisjointToBatchedAttributes(Layer):
+    Reconstructs batched tensor with the help of ID tensor information.
+    """
 
-    def __init__(self, reverse_indices: bool = False, dtype_batch: str = "int64", dtype_index=None,
-                 padded_disjoint: bool = False, **kwargs):
+    def __init__(self, **kwargs):
         super(CastDisjointToBatchedAttributes, self).__init__(**kwargs)
-        self.reverse_indices = reverse_indices
-        self.dtype_batch = dtype_batch
-        self.padded_disjoint = padded_disjoint
-        self.supports_jit = padded_disjoint
-        self.dtype_index = dtype_index
 
     def build(self, input_shape):
         self.built = True
@@ -363,12 +386,15 @@ class CastDisjointToBatchedAttributes(Layer):
         Args:
             inputs (list): List of `[target, attr, graph_id_attr, attr_id]` ,
 
-                - attr (Tensor): Features are represented by a keras tensor of shape `(batch, N, F, ...)` ,
+                - target (Tensor): Target tensor of shape equal to desired output of shape `(batch, N, F, ...)` .
+                  Only first dimension is required for making new tensor e.g. of shape  `(batch,)` .
+                - attr (Tensor): Features are represented by a keras tensor of shape `([N], F, ...)` ,
                   where N denotes the number of nodes or edges.
-                - total_attr (Tensor):
+                - graph_id_attr (Tensor): ID tensor of graph assignment in disjoint graph of shape `([N], )` .
+                - attr_id (Tensor): The ID-tensor to assign each node to its respective graph of shape `([N], )` .
 
         Returns:
-            Tensor:
+            Tensor: Batched output tensor of node or edge attributes of shape `(batch, N, F, ...)` .
         """
         target, attr, graph_id_attr, attr_id = inputs
 
@@ -387,23 +413,17 @@ class CastDisjointToBatchedAttributes(Layer):
             out = ops.reshape(out, list(ops.shape(target)[:2]) + list(ops.shape(attr)[1:]))
         return out
 
-    def get_config(self):
-        """Get config dictionary for this layer."""
-        config = super(CastDisjointToBatchedAttributes, self).get_config()
-        config.update({"dtype_batch": self.dtype_batch})
-        return config
 
+class CastGraphStateToDisjoint(_CastDisjointBase):
+    r"""Cast graph property tensor to disjoint graph representation of
+    `Pytorch Geometric (PyG) <https://github.com/pyg-team/pytorch_geometric>`__ .
 
-class CastGraphStateToDisjoint(Layer):
+    The graph state is usually kept as batched tensor, except for padded disjoint representation, an empty zero valued
+    graph is added to represent all padded nodes.
+    """
 
-    def __init__(self, reverse_indices: bool = False, dtype_batch: str = "int64", dtype_index=None,
-                 padded_disjoint: bool = False, **kwargs):
+    def __init__(self, **kwargs):
         super(CastGraphStateToDisjoint, self).__init__(**kwargs)
-        self.reverse_indices = reverse_indices
-        self.dtype_batch = dtype_batch
-        self.dtype_index = dtype_index
-        self.supports_jit = True
-        self.padded_disjoint = padded_disjoint
 
     def build(self, input_shape):
         self.built = True
@@ -421,14 +441,8 @@ class CastGraphStateToDisjoint(Layer):
             inputs (Tensor): Graph labels from a disjoint representation of shape `(batch, ...)` .
 
         Returns:
-            Tensor: Graph labels of shape `(batch, ...)` .
+            Tensor: Graph labels of shape `(batch, ...)` or `(batch+1, ...)` for padded disjoint.
         """
         if self.padded_disjoint:
             return pad_left(inputs)
         return inputs
-
-    def get_config(self):
-        """Get config dictionary for this layer."""
-        config = super(CastGraphStateToDisjoint, self).get_config()
-        config.update({"dtype_batch": self.dtype_batch})
-        return config
--- kgcnn/layers/gather.py
+++ kgcnn/layers/gather.py
@@ -108,63 +108,151 @@ class GatherNodes(Layer):
 
 
 class GatherNodesOutgoing(Layer):
+    r"""Gather sending or outgoing nodes of edges with index :math:`j` .
+
+    An edge is defined by index tuple :math:`(i, j)`.
+    In the default definition, index :math:`j` is expected to be the sending or source node.
+    """
 
     def __init__(self, selection_index: int = global_index_send, axis_indices: int = global_axis_indices, **kwargs):
+        """Initialize layer.
+
+        Args:
+            selection_index (int): Index of sending nodes. Default is 1.
+            axis_indices (int): Axis of node indices in index Tensor. Default is 0.
+        """
         super(GatherNodesOutgoing, self).__init__(**kwargs)
         self.selection_index = selection_index
         self.axis_indices = axis_indices
 
     def build(self, input_shape):
+        """Build layer."""
         super(GatherNodesOutgoing, self).build(input_shape)
 
     def compute_output_shape(self, input_shape):
+        """Compute output shape of this layer."""
         assert len(input_shape) == 2
         x_shape, indices_shape = list(input_shape[0]),  list(input_shape[1])
         indices_shape.pop(self.axis_indices)
         return tuple(indices_shape + x_shape[1:])
 
     def call(self, inputs, **kwargs):
+        r"""Forward pass.
+
+        Args:
+            inputs (list): [nodes, index]
+
+                - nodes (Tensor): Node embeddings of shape `([N], F)`
+                - index (Tensor): Edge indices referring to nodes of shape `(2, [M])`
+
+        Returns:
+            Tensor: Gathered node embeddings that match the number of edges of shape `([M], F)` .
+        """
         x, index = inputs
         indices_take = ops.take(index, self.selection_index, axis=self.axis_indices)
         return ops.take(x, indices_take, axis=0)
 
+    def get_config(self):
+        """Get config for this layer."""
+        conf = super(GatherNodesOutgoing, self).get_config()
+        conf.update({"selection_index": self.selection_index, "axis_indices": self.axis_indices})
+        return conf
+
 
 class GatherNodesIngoing(Layer):
+    r"""Gather receiving or ingoing nodes of edges with index :math:`i` .
+
+    An edge is defined by index tuple :math:`(i, j)`.
+    In the default definition, index :math:`i` is expected to be the receiving or target node.
+    """
 
     def __init__(self, selection_index: int = global_index_receive, axis_indices: int = global_axis_indices, **kwargs):
+        """Initialize layer.
+
+        Args:
+            selection_index (int): Index of receiving nodes. Default is 0.
+            axis_indices (int): Axis of node indices in index Tensor. Default is 0.
+        """
         super(GatherNodesIngoing, self).__init__(**kwargs)
         self.selection_index = selection_index
         self.axis_indices = axis_indices
 
     def build(self, input_shape):
+        """Build layer."""
         super(GatherNodesIngoing, self).build(input_shape)
 
     def compute_output_shape(self, input_shape):
+        """Compute output shape of this layer."""
         assert len(input_shape) == 2
         x_shape, indices_shape = list(input_shape[0]),  list(input_shape[1])
         indices_shape.pop(self.axis_indices)
         return tuple(indices_shape + x_shape[1:])
 
     def call(self, inputs, **kwargs):
+        r"""Forward pass.
+
+        Args:
+            inputs (list): [nodes, index]
+
+                - nodes (Tensor): Node embeddings of shape `([N], F)`
+                - index (Tensor): Edge indices referring to nodes of shape `(2, [M])`
+
+        Returns:
+            Tensor: Gathered node embeddings that match the number of edges of shape `([M], F)` .
+        """
         x, index = inputs
         indices_take = ops.take(index, self.selection_index, axis=self.axis_indices)
         return ops.take(x, indices_take, axis=0)
 
+    def get_config(self):
+        """Get config for this layer."""
+        conf = super(GatherNodesIngoing, self).get_config()
+        conf.update({"selection_index": self.selection_index, "axis_indices": self.axis_indices})
+        return conf
+
 
 class GatherState(Layer):
+    r"""Layer to repeat environment or global state for a specific embeddings tensor like node or edge lists.
+
+    To repeat the correct global state (like an environment feature vector) for each sub graph,
+    a tensor with the target shape and batch ID is required.
+
+    Mostly used to concatenate a global state :math:`\mathbf{s}` with node embeddings :math:`\mathbf{h}_i`
+    like for example:
+
+    .. math::
+
+        \mathbf{h}_i = \mathbf{h}_i \oplus \mathbf{s}
+
+    where this layer only repeats :math:`\mathbf{s}` to match an embedding tensor :math:`\mathbf{h}_i`.
+    """
 
     def __init__(self, **kwargs):
+        """Initialize layer."""
         super(GatherState, self).__init__(**kwargs)
 
     def build(self, input_shape):
+        """Build layer."""
         super(GatherState, self).build(input_shape)
 
     def compute_output_shape(self, input_shape):
+        """Compute output shape of this layer."""
         assert len(input_shape) == 2
         state_shape, id_shape = list(input_shape[0]),  list(input_shape[1])
         return tuple(id_shape + state_shape[1:])
 
     def call(self, inputs, **kwargs):
+        r"""Forward pass.
+
+        Args:
+            inputs: [state, batch_id]
+
+                - state (Tensor): Graph specific embedding tensor. This is tensor of shape `(batch, F)`
+                - batch_id (Tensor): Tensor of batch IDs for each sub-graph of shape `([N], )` .
+
+        Returns:
+            Tensor: Graph embedding with repeated single state for each sub-graph of shape `([N], F)`.
+        """
         env, batch_id = inputs
         out = ops.take(env, batch_id, axis=0)
         return out
@@ -211,6 +299,7 @@ class GatherEdgesPairs(Layer):
         return edges_corrected
 
     def get_config(self):
+        """Get layer config."""
         conf = super(GatherEdgesPairs, self).get_config()
         conf.update({"axis_indices": self.axis_indices})
         return conf
