7fd945176445280cd8dd1a663df50afeb8e6de4b
PatReis <pat.reiser@web.de>
2023-11-20 18:44:15 +0100

    update for keras 3.0

--- kgcnn/literature/GCN/__init__.py
+++ kgcnn/literature/GCN/__init__.py
@@ -1,6 +1,10 @@
 from ._make import make_model, model_default
+from ._make import make_model_weighted, model_default_weighted
 
 __all__ = [
     "make_model",
-    "model_default"
+    "model_default",
+    "make_model_weighted",
+    "model_default_weighted"
 ]
+
