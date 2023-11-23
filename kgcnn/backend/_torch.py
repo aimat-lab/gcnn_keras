2db7ab6b8f45ed2e9ae7702c6abdf2b940f7dc6a
PatReis <pat.reiser@web.de>
2023-11-23 17:05:46 +0100

    update for keras 3.0

--- kgcnn/backend/_torch.py
+++ kgcnn/backend/_torch.py
@@ -2,31 +2,42 @@ import torch
 
 
 def scatter_reduce_sum(indices, values, shape):
-    indices = torch.unsqueeze(indices, dim=-1)
+    dims_to_add = values.dim() - indices.dim()
+    for _ in range(dims_to_add):
+        indices = torch.unsqueeze(indices, dim=-1)
     return torch.zeros(*shape, dtype=values.dtype, device=values.device).scatter_reduce(
         0, torch.broadcast_to(indices, values.shape), values, reduce='sum')
 
 
 def scatter_reduce_min(indices, values, shape):
-    indices = torch.unsqueeze(indices, dim=-1)
+    dims_to_add = values.dim() - indices.dim()
+    for _ in range(dims_to_add):
+        indices = torch.unsqueeze(indices, dim=-1)
     return torch.zeros(*shape, dtype=values.dtype, device=values.device).scatter_reduce(
         0, torch.broadcast_to(indices, values.shape), values, reduce='amin', include_self=False)
 
 
 def scatter_reduce_max(indices, values, shape):
-    indices = torch.unsqueeze(indices, dim=-1)
+    dims_to_add = values.dim() - indices.dim()
+    for _ in range(dims_to_add):
+        indices = torch.unsqueeze(indices, dim=-1)
     return torch.zeros(*shape, dtype=values.dtype, device=values.device).scatter_reduce(
         0, torch.broadcast_to(indices, values.shape), values, reduce='amax', include_self=False)
 
 
 def scatter_reduce_mean(indices, values, shape):
-    indices = torch.unsqueeze(indices, dim=-1)
+    dims_to_add = values.dim() - indices.dim()
+    for _ in range(dims_to_add):
+        indices = torch.unsqueeze(indices, dim=-1)
     return torch.zeros(*shape, dtype=values.dtype, device=values.device).scatter_reduce(
         0, torch.broadcast_to(indices, values.shape), values, reduce='mean', include_self=False)
 
 
 def scatter_reduce_softmax(indices, values, shape, normalize: bool = False):
-    indices_scatter = torch.unsqueeze(indices, dim=-1)
+    indices_scatter = indices
+    dims_to_add = values.dim()-indices.dim()
+    for _ in range(dims_to_add):
+        indices_scatter = torch.unsqueeze(indices_scatter, dim=-1)
 
     if normalize:
         zeros_min = torch.zeros(*shape, dtype=values.dtype, device=values.device)
